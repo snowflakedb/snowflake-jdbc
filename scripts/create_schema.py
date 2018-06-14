2bba2c53adccfecdd3afcf00cc67f89d438a1fee
Shige Takeda <smtakeda@gmail.com>
2018-06-14 12:24:31 -0700

    Add Build on Travis (#32)
    * Add build on travis

---
+++ scripts/create_schema.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# Create test schema
+#
+import os
+import sys
+import snowflake.connector
+
+travis_job_id = os.getenv('TRAVIS_JOB_ID')
+appveyor_job_id = os.getenv('APPVEYOR_BUILD_ID')
+if not travis_job_id and not appveyor_job_id:
+    print("[WARN] The environment variable TRAVIS_JOB_ID or APPVEYOR_BUILD_ID is not set. No test schema will be created.")
+    sys.exit(1)
+
+if travis_job_id:
+  test_schema = 'TRAVIS_JOB_{0}'.format(travis_job_id)
+else:
+  test_schema = 'APPVEYOR_BUILD_{0}'.format(appveyor_job_id)
+  
+params = {
+    'account': os.getenv("SNOWFLAKE_TEST_ACCOUNT"),
+    'user': os.getenv("SNOWFLAKE_TEST_USER"),
+    'password': os.getenv("SNOWFLAKE_TEST_PASSWORD"),
+    'database': os.getenv("SNOWFLAKE_TEST_DATABASE"),
+    'role': os.getenv("SNOWFLAKE_TEST_ROLE"),
+}
+host=os.getenv("SNOWFLAKE_TEST_HOST")
+if host:
+    params['host'] = host
+port=os.getenv("SNOWFLAKE_TEST_PORT")
+if port:
+    params['port'] = port
+protocol=os.getenv("SNOWFLAKE_TEST_PROTOCOL")
+if protocol:
+    params['protocol'] = protocol
+
+con = snowflake.connector.connect(**params)
+con.cursor().execute("create or replace schema {0}".format(test_schema))
+
+sys.exit(0)
---
+++ scripts/run_travis.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+#
+# Run Travis Build and Tests
+#
+set -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+function travis_fold_start() {
+    local name=$1
+    local message=$2
+    echo "travis_fold:start:$name"
+    tput setaf 3 || true
+    echo $message
+    tput sgr0 || true
+    export travis_fold_name=$name
+}
+
+function travis_fold_end() {
+    echo "travis_fold:end:$travis_fold_name"
+    unset travis_fold_name
+}
+
+function finish {
+    travis_fold_start drop_schema "Drop test schema"
+    python $DIR/drop_schema.py 
+    travis_fold_end
+}
+
+travis_fold_start pythonvenv "Set up Python Virtualenv (pyenv)"
+pip install -U snowflake-connector-python
+travis_fold_end
+
+trap finish EXIT
+
+source $DIR/env.sh
+
+travis_fold_start create_schema "Create test schema"
+python $DIR/create_schema.py
+travis_fold_end
+
+travis_fold_start build "Build JDBC driver"
+mvn install -DskipTests=true --batch-mode --show-version
+travis_fold_end
+
