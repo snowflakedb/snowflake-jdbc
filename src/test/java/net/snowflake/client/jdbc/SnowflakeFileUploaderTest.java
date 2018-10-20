88af726acba365379768f99a4040b6005230c0ae
howryu <yuhaowei@umich.edu>
2018-10-20 12:45:02 -0700

    Enable FileUploaderTest

---
+++ src/test/java/net/snowflake/client/jdbc/SnowflakeFileUploaderTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2012-2018 Snowflake Computing Inc. All right reserved.
+ */
+package net.snowflake.client.jdbc;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
+import java.util.TimeZone;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jhuang
+ */
+public class SnowflakeFileUploaderTest
+{
+  @Rule
+  public TemporaryFolder folder = new TemporaryFolder();
+
+  @Test
+  public void testProcessFileNames() throws Exception
+  {
+    folder.newFile("TestFileA");
+    folder.newFile("TestFileB");
+
+    String folderName = folder.getRoot().getCanonicalPath();
+    System.setProperty("user.dir", folderName);
+    System.setProperty("user.home", folderName);
+
+    String[] locations =
+        {
+            folderName + "/Tes*Fil*A",
+            folderName + "/TestFil?B",
+            "~/TestFileC", "TestFileD"
+        };
+
+    Set<String> files = SnowflakeFileTransferAgent.expandFileNames(locations);
+
+    assertTrue(files.contains(folderName + "/TestFileA"));
+    assertTrue(files.contains(folderName + "/TestFileB"));
+    assertTrue(files.contains(folderName + "/TestFileC"));
+    assertTrue(files.contains(folderName + "/TestFileD"));
+  }
+}
