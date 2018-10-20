1193a4711dc4f7f9ad78b79882b5b25188c9aa01
howryu <yuhaowei@umich.edu>
2018-10-20 12:22:05 -0700

    Enable more tests

---
+++ src/test/java/net/snowflake/client/jdbc/RestRequestIT.java
@@ -0,0 +1,142 @@
+package net.snowflake.client.jdbc;
+
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class RestRequestIT
+{
+  private CloseableHttpResponse retryResponse()
+  {
+    StatusLine retryStatusLine = mock(StatusLine.class);
+    when(retryStatusLine.getStatusCode())
+        .thenReturn(503);
+
+    CloseableHttpResponse retryResponse = mock(CloseableHttpResponse.class);
+    when(retryResponse.getStatusLine())
+        .thenReturn(retryStatusLine);
+
+    return retryResponse;
+  }
+
+  private CloseableHttpResponse successResponse()
+  {
+    StatusLine successStatusLine = mock(StatusLine.class);
+    when(successStatusLine.getStatusCode())
+        .thenReturn(200);
+
+    CloseableHttpResponse successResponse = mock(CloseableHttpResponse.class);
+    when(successResponse.getStatusLine())
+        .thenReturn(successStatusLine);
+
+    return successResponse;
+  }
+
+  private void execute(CloseableHttpClient client, String uri,
+                       boolean includeRetryParameters)
+      throws IOException, SnowflakeSQLException
+  {
+    RestRequest.execute(
+        client,
+        new HttpGet(uri),
+        0,
+        0,
+        new AtomicBoolean(false),
+        false,
+        includeRetryParameters,
+        true);
+  }
+
+  @Test
+  public void testRetryParamsInRequest() throws IOException, SnowflakeSQLException
+  {
+    CloseableHttpClient client = mock(CloseableHttpClient.class);
+    when(client.execute(any(HttpUriRequest.class)))
+        .thenAnswer(new Answer<CloseableHttpResponse>()
+        {
+          int callCount = 0;
+
+          @Override
+          public CloseableHttpResponse answer(InvocationOnMock invocation)
+              throws Throwable
+          {
+            HttpUriRequest arg = (HttpUriRequest) invocation.getArguments()[0];
+            String params = arg.getURI().getQuery();
+
+            if (callCount == 0)
+            {
+              assertFalse(params.contains("retryCount="));
+              assertFalse(params.contains("clientStartTime="));
+              assertTrue(params.contains("request_guid="));
+            }
+            else
+            {
+              assertTrue(params.contains("retryCount=" + callCount));
+              assertTrue(params.contains("clientStartTime="));
+              assertTrue(params.contains("request_guid="));
+            }
+
+            callCount += 1;
+            if (callCount >= 3)
+            {
+              return successResponse();
+            }
+            else
+            {
+              return retryResponse();
+            }
+          }
+        });
+
+   execute(client, "fakeurl.com/?requestId=abcd-1234", true);
+  }
+
+  @Test
+  public void testRetryNoParamsInRequest() throws IOException, SnowflakeSQLException
+  {
+    CloseableHttpClient client = mock(CloseableHttpClient.class);
+    when(client.execute(any(HttpUriRequest.class)))
+        .thenAnswer(new Answer<CloseableHttpResponse>()
+        {
+          int callCount = 0;
+
+          @Override
+          public CloseableHttpResponse answer(InvocationOnMock invocation)
+              throws Throwable
+          {
+            HttpUriRequest arg = (HttpUriRequest) invocation.getArguments()[0];
+            String params = arg.getURI().getQuery();
+
+            if (callCount > 0)
+            {
+              assertTrue(params.contains("retryCount=" + callCount));
+            }
+            assertFalse(params.contains("clientStartTime="));
+            assertTrue(params.contains("request_guid="));
+
+            callCount += 1;
+            if (callCount >= 3)
+            {
+              return successResponse();
+            }
+            else
+            {
+              return retryResponse();
+            }
+          }
+        });
+
+    execute(client, "fakeurl.com/?requestId=abcd-1234", false);
+  }
+}
