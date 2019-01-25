d1d4009fabfbe01f27cfa88881441b56ff1af21b
Shige Takeda <smtakeda@gmail.com>
2019-01-25 22:26:50 +0000

    SNOW-64564: Initialize logger onlhy if tracing parameter is set, otherwise JDBC driver will let  application code to enable logging. Removed slf4j auto detection.

--- src/main/java/net/snowflake/client/log/SFLoggerFactory.java
+++ src/main/java/net/snowflake/client/log/SFLoggerFactory.java
@@ -3,10 +3,6 @@
  */
 package net.snowflake.client.log;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Enumeration;
-
 /**
  * Used to create SFLogger instance
  *
@@ -14,7 +10,7 @@ import java.util.Enumeration;
  */
 public class SFLoggerFactory
 {
-  public static LoggerImpl loggerImplementation;
+  private static LoggerImpl loggerImplementation;
 
   enum LoggerImpl
   {
@@ -64,14 +60,8 @@ public class SFLoggerFactory
       loggerImplementation = LoggerImpl.fromString(logger);
 
       if (loggerImplementation == null) {
-        // try to load slf4j implementation class first
-        loggerImplementation = slf4jImplExist() ? LoggerImpl.SLF4JLOGGER
-            : LoggerImpl.JDK14LOGGER;
-      }
-
-      if (loggerImplementation == LoggerImpl.JDK14LOGGER)
-      {
-        JDK14Logger.defaultInit();
+        // default to use java util logging
+        loggerImplementation = LoggerImpl.JDK14LOGGER;
       }
     }
 
@@ -83,38 +73,5 @@ public class SFLoggerFactory
       default:
         return new JDK14Logger(clazz.getName());
     }
-  }
-
-  private static final String STATIC_LOGGER_BINDER_PATH="org/slf4j/impl/StaticLoggerBinder.class";
-  /**
-   * Methods used to determine if the slf4j implementation exist
-   * or not.
-   *
-   * The way to determine is to figure out if org.slf4j.StaticLoggerBinder
-   * is in the classpath or not. If not, then there is no implementation,
-   * switch to JDKLogger by default;
-   * @return
-   */
-  private static boolean slf4jImplExist()
-  {
-    try
-    {
-      ClassLoader loggerFactoryClassLoader = SFLoggerFactory.class.getClassLoader();
-      Enumeration<URL> paths;
-      if (loggerFactoryClassLoader == null)
-      {
-        paths = ClassLoader.getSystemResources(STATIC_LOGGER_BINDER_PATH);
-      }
-      else
-      {
-        paths = loggerFactoryClassLoader.getResources(STATIC_LOGGER_BINDER_PATH);
-      }
-      return paths.hasMoreElements();
-    }
-    catch (IOException e)
-    {
-      System.err.println(e);
-    }
-    return false;
   }
 }
