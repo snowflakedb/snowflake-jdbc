69c88b163a13e79c825d65484ca32da457f58d66
Shige Takeda <smtakeda@gmail.com>
2019-02-01 23:30:45 +0000

    SNOW-65947: Internal change for future improvements

--- src/main/java/net/snowflake/client/jdbc/telemetryV2/TelemetryEvent.java
+++ src/main/java/net/snowflake/client/jdbc/telemetryV2/TelemetryEvent.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.TimeZone;
+import java.util.UUID;
 
 public class TelemetryEvent extends JSONObject
 {
@@ -190,6 +191,7 @@ public class TelemetryEvent extends JSONObject
 
     protected TelemetryEvent build()
     {
+      body.put("UUID", UUID.randomUUID().toString());
       body.put("Created_On", getUTCNow());
       this.putMap("Tags", tags);
       return body;
