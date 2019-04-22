2085246331020ab1ab9893ad8145e7ad9ff6dea7
Shige Takeda <smtakeda@gmail.com>
2019-04-22 15:37:58 +0000

    SNOW-74492: Split LoaderIT

--- src/test/java/net/snowflake/client/loader/LoaderIT.java
+++ src/test/java/net/snowflake/client/loader/LoaderIT.java
@@ -3,13 +3,9 @@
  */
 package net.snowflake.client.loader;
 
-import net.snowflake.client.AbstractDriverIT;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -20,7 +16,6 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
 import java.util.TimeZone;
 
@@ -36,42 +31,8 @@ import static org.junit.Assert.fail;
 /**
  * Loader IT
  */
-public class LoaderIT
+public class LoaderIT extends LoaderBase
 {
-  private final static String TARGET_TABLE_NAME = "LOADER_test_TABLE";
-
-  private static Connection testConnection;
-  private static Connection putConnection;
-  private static String SCHEMA_NAME;
-
-  @BeforeClass
-  public static void setUpClass() throws Throwable
-  {
-    testConnection = AbstractDriverIT.getConnection();
-    putConnection = AbstractDriverIT.getConnection();
-
-    SCHEMA_NAME = testConnection.getSchema();
-    testConnection.createStatement().execute(String.format(
-        "CREATE OR REPLACE TABLE \"%s\" ("
-        + "ID int, "
-        + "C1 varchar(255), "
-        + "C2 varchar(255) DEFAULT 'X', "
-        + "C3 double, "
-        + "C4 timestamp, "
-        + "C5 variant)",
-        LoaderIT.TARGET_TABLE_NAME));
-  }
-
-  @AfterClass
-  public static void tearDownClass() throws SQLException
-  {
-    testConnection.createStatement().execute(
-        String.format("DROP TABLE IF EXISTS \"%s\"", LoaderIT.TARGET_TABLE_NAME));
-
-    testConnection.close();
-    putConnection.close();
-  }
-
   @Test
   public void testInjectBadStagedFileInsert() throws Exception
   {
@@ -185,51 +146,6 @@ public class LoaderIT
     // PUT retry
     new TestDataConfigBuilder(testConnection, putConnection)
         .setTestMode(true).populate();
-  }
-
-  @Test
-  public void testLoaderMultipleBatch() throws Exception
-  {
-    String refTableName = "LOADER_TEST_TABLE_REF";
-    testConnection.createStatement().execute(String.format(
-        "CREATE OR REPLACE TABLE \"%s\" ("
-        + "ID int, "
-        + "C1 varchar(255), "
-        + "C2 varchar(255) DEFAULT 'X', "
-        + "C3 double, "
-        + "C4 timestamp, "
-        + "C5 variant)", refTableName));
-
-    try
-    {
-      TestDataConfigBuilder tdcb = new TestDataConfigBuilder(
-          testConnection, putConnection);
-      List<Object[]> dataSet = tdcb.populateReturnData();
-
-      TestDataConfigBuilder tdcbRef = new TestDataConfigBuilder(
-          testConnection, putConnection);
-      tdcbRef.setDataSet(dataSet)
-          .setTableName(refTableName)
-          .setCsvFileBucketSize(2)
-          .setCsvFileSize(30000).populate();
-
-      ResultSet rsReference = testConnection.createStatement().executeQuery(String.format(
-          "SELECT hash_agg(*) FROM \"%s\"", TARGET_TABLE_NAME
-      ));
-      rsReference.next();
-      long hashValueReference = rsReference.getLong(1);
-      ResultSet rsTarget = testConnection.createStatement().executeQuery(String.format(
-          "SELECT hash_agg(*) FROM \"%s\"", refTableName
-      ));
-      rsTarget.next();
-      long hashValueTarget = rsTarget.getLong(1);
-      assertThat("hash values", hashValueTarget, equalTo(hashValueReference));
-    }
-    finally
-    {
-      testConnection.createStatement().execute(String.format(
-          "DROP TABLE IF EXISTS %s", refTableName));
-    }
   }
 
   @Test
@@ -681,162 +597,6 @@ public class LoaderIT
     assertThat("C3. No commit should happen",
                Double.toHexString((rs.getDouble("C3"))),
                equalTo("0x1.044cc0000225cp4"));
-  }
-
-  @Test
-  public void testLoadTimestampV1() throws Exception
-  {
-    final String targetTableName = "LOADER_TEST_TIMESTAMP_V1";
-
-    // create table including TIMESTAMP_NTZ
-    testConnection.createStatement().execute(String.format(
-        "CREATE OR REPLACE TABLE %s ("
-        + "ID int, "
-        + "C1 varchar(255), "
-        + "C2 timestamp_ntz)", targetTableName));
-
-    // Binding java.sql.Time with TIMESTAMP is supported only if
-    // mapTimeToTimestamp flag is enabled. This is required to keep the
-    // old behavior of Informatica V1 connector.
-    Object[] testData = new Object[]{
-        // full timestamp in Time object. Interestingly all values are
-        // preserved.
-        new java.sql.Time(1502931205000L),
-        java.sql.Time.valueOf("12:34:56") // a basic test case
-    };
-
-    for (int i = 0; i < 2; ++i)
-    {
-      boolean useLocalTimezone;
-      TimeZone originalTimeZone;
-      TimeZone targetTimeZone;
-
-      if (i == 0)
-      {
-        useLocalTimezone = true;
-        originalTimeZone = TimeZone.getDefault();
-        targetTimeZone = TimeZone.getTimeZone("America/Los_Angeles");
-      }
-      else
-      {
-        useLocalTimezone = false;
-        originalTimeZone = TimeZone.getTimeZone("UTC");
-        targetTimeZone = TimeZone.getTimeZone("UTC");
-      }
-
-      // input timestamp associated with the target timezone, America/Los_Angeles
-      for (Object testTs : testData)
-      {
-        _testLoadTimestamp(targetTableName, originalTimeZone,
-                           targetTimeZone, testTs, useLocalTimezone, true);
-      }
-    }
-  }
-
-  @Test
-  public void testLoadTimestamp() throws Exception
-  {
-    final String targetTableName = "LOADER_TEST_TIMESTAMP";
-
-    // create table including TIMESTAMP_NTZ
-    testConnection.createStatement().execute(String.format(
-        "CREATE OR REPLACE TABLE %s ("
-        + "ID int, "
-        + "C1 varchar(255), "
-        + "C2 timestamp_ntz)", targetTableName));
-
-    // Binding java.util.Date, Timestamp and java.sql.Date with TIMESTAMP
-    // datatype. No java.sql.Time binding is supported for TIMESTAMP.
-    // For java.sql.Time, the target data type must be TIME.
-    Object[] testData = new Object[]{
-        new Date(),
-        java.sql.Timestamp.valueOf("0001-01-01 08:00:00"),
-        java.sql.Date.valueOf("2001-01-02")
-    };
-
-    for (int i = 0; i < 2; ++i)
-    {
-      boolean useLocalTimezone = false;
-      TimeZone originalTimeZone;
-      TimeZone targetTimeZone;
-
-      if (i == 0)
-      {
-        useLocalTimezone = true;
-        originalTimeZone = TimeZone.getDefault();
-        targetTimeZone = TimeZone.getTimeZone("America/Los_Angeles");
-      }
-      else
-      {
-        originalTimeZone = TimeZone.getTimeZone("UTC");
-        targetTimeZone = TimeZone.getTimeZone("UTC");
-      }
-
-      // input timestamp associated with the target timezone, America/Los_Angeles
-      for (Object testTs : testData)
-      {
-        _testLoadTimestamp(targetTableName, originalTimeZone,
-                           targetTimeZone, testTs, useLocalTimezone, false);
-      }
-    }
-  }
-
-  private void _testLoadTimestamp(
-      String targetTableName,
-      TimeZone originalTimeZone, TimeZone targetTimeZone,
-      Object testTs, boolean useLocalTimeZone,
-      boolean mapTimeToTimestamp) throws Exception
-  {
-    TestDataConfigBuilder tdcb = new TestDataConfigBuilder(
-        testConnection, putConnection);
-
-    tdcb
-        .setStartTransaction(true)
-        .setTruncateTable(true)
-        .setTableName(targetTableName)
-        .setUseLocalTimezone(useLocalTimeZone)
-        .setMapTimeToTimestamp(mapTimeToTimestamp)
-        .setColumns(Arrays.asList(
-            "ID", "C1", "C2"
-        ));
-    StreamLoader loader = tdcb.getStreamLoader();
-    TestDataConfigBuilder.ResultListener listener = tdcb.getListener();
-
-    TimeZone.setDefault(targetTimeZone); // change default timezone before start
-
-    loader.start();
-
-    for (int i = 0; i < 5; ++i)
-    {
-      Object[] row = new Object[]{
-          i, "foo_" + i, testTs
-      };
-      loader.submitRow(row);
-    }
-    loader.finish();
-    TimeZone.setDefault(originalTimeZone);
-
-    assertThat("Loader detected errors",
-               listener.getErrorCount(), equalTo(0));
-
-    ResultSet rs = testConnection.createStatement().executeQuery(
-        String.format("SELECT * FROM \"%s\"", targetTableName));
-
-    rs.next();
-    Timestamp ts = rs.getTimestamp("C2");
-
-    // format the input TS with the target timezone
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-    sdf.setTimeZone(targetTimeZone);
-    String currenTsStr = sdf.format(testTs);
-
-    // format the retrieved TS with the original timezone
-    sdf.setTimeZone(originalTimeZone);
-    String retrievedTsStr = sdf.format(new Date(ts.getTime()));
-
-    // They must be identical.
-    assertThat("Input and retrieved timestamp are different",
-               retrievedTsStr, equalTo(currenTsStr));
   }
 
   @Test
