0bf2f25c10122b1888f16d0c1326772b06b4c85a
Shige Takeda <shige.takeda@snowflake.com>
2019-06-17 11:32:44 -0400

    Format source code

--- src/test/java/net/snowflake/client/jdbc/PreparedStatementIT.java
+++ src/test/java/net/snowflake/client/jdbc/PreparedStatementIT.java
@@ -53,7 +53,7 @@ public class PreparedStatementIT extends BaseJDBCTest
   public static Object[][] data()
   {
     // all tests in this class need to run for both query result formats json and arrow
-    return new Object[][] {
+    return new Object[][]{
         {"JSON"},
         {"Arrow"}
     };
