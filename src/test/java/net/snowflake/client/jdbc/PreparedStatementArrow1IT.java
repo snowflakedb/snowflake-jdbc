a2b52b30a23d408282dad4e5205723da1211f73a
Shige Takeda <shige.takeda@snowflake.com>
2019-11-04 00:26:06 -0800

    SNOW-105740: split PreparedStatementIT

---
+++ src/test/java/net/snowflake/client/jdbc/PreparedStatementArrow1IT.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2012-2019 Snowflake Computing Inc. All right reserved.
+ */
+package net.snowflake.client.jdbc;
+
+/**
+ * Test PreparedStatement in ARROW format 2/2
+ */
+public class PreparedStatementArrow1IT extends PreparedStatement1IT
+{
+  public PreparedStatementArrow1IT()
+  {
+    super("arrow");
+  }
+}
---
+++ src/test/java/net/snowflake/client/jdbc/PreparedStatementArrow2IT.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2012-2019 Snowflake Computing Inc. All right reserved.
+ */
+package net.snowflake.client.jdbc;
+
+/**
+ * Test PreparedStatement in ARROW format 2/2
+ */
+public class PreparedStatementArrow2IT extends PreparedStatement2IT
+{
+  public PreparedStatementArrow2IT()
+  {
+    super("arrow");
+  }
+}
