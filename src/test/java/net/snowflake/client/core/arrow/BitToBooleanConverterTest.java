47345a865d1d115f113adff83f17014ba81cad3b
Shige Takeda <shige.takeda@snowflake.com>
2019-11-22 13:45:17 -0800

    SNOW-115108: fix arrow data not allocating more than 2GB

--- src/test/java/net/snowflake/client/core/arrow/BitToBooleanConverterTest.java
+++ src/test/java/net/snowflake/client/core/arrow/BitToBooleanConverterTest.java
@@ -25,7 +25,7 @@ public class BitToBooleanConverterTest extends BaseConverterTest
   /**
    * allocator for arrow
    */
-  private BufferAllocator allocator = new RootAllocator(Integer.MAX_VALUE);
+  private BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
 
   private Random random = new Random();
 
