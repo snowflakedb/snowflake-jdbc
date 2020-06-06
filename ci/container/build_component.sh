18f2eede03ebc05863a18a04c49e1dd25f28e2d4
Shige Takeda <shige.takeda@snowflake.com>
2020-06-06 03:11:00 +0000

    batch mode

--- ci/container/build_component.sh
+++ ci/container/build_component.sh
@@ -8,8 +8,8 @@ JDBC_ROOT=$(cd "${THIS_DIR}/../../" && pwd)
 
 cd $JDBC_ROOT
 rm -f lib/*.jar
-mvn clean install
+mvn clean install --batch-mode --show-version
 cd FIPS
 rm -f lib/*.jar
-mvn clean install
+mvn clean install --batch-mode --show-version
 $THIS_DIR/upload_artifact.sh
