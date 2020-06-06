0d0b5174cbb2a7a7e509d53710fa064b5ef3ec76
Shige Takeda <shige.takeda@snowflake.com>
2020-06-06 04:16:22 +0000

    fixed path

--- ci/container/download_artifact.sh
+++ ci/container/download_artifact.sh
@@ -2,19 +2,29 @@
 #
 # Download Artifact
 #
-export GIT_BRANCH=${GIT_BRANCH:-origin/$(git rev-parse --abbrev-ref HEAD)}
-export GIT_COMMIT=${GIT_COMMIT:-$(git rev-parse HEAD)}
+set -o pipefail
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+JDBC_ROOT=$(cd "${THIS_DIR}/../../" && pwd)
 
-BRANCH=$(basename ${GIT_BRANCH})
+if [[ -z "$GITHUB_ACTIONS" ]] ;then
+    export GIT_BRANCH=${GIT_BRANCH:-origin/$(git rev-parse --abbrev-ref HEAD)}
+    export GIT_COMMIT=${GIT_COMMIT:-$(git rev-parse HEAD)}
 
-# Place to hold downloaded library
-export LIB_DIR=$WORKSPACE/lib
+    BRANCH=$(basename ${GIT_BRANCH})
 
-mkdir -p $LIB_DIR
-pushd $LIB_DIR >& /dev/null
-    source_stage=s3://sfc-jenkins/repository/jdbc/${BRANCH}/${GIT_COMMIT}
-    echo "[INFO] downloading ${source_stage}/"
-    aws s3 cp --only-show-errors $source_stage/ . --recursive
-popd >& /dev/null
-mkdir -p /mnt/host/lib
-cp -p $LIB_DIR/*.jar /mnt/host/lib
+    # Place to hold downloaded library
+    export LIB_DIR=$WORKSPACE/lib
+
+    mkdir -p $LIB_DIR
+    pushd $LIB_DIR >& /dev/null
+        source_stage=s3://sfc-jenkins/repository/jdbc/${BRANCH}/${GIT_COMMIT}
+        echo "[INFO] downloading ${source_stage}/"
+        aws s3 cp --only-show-errors $source_stage/ . --recursive
+    popd >& /dev/null
+    mkdir -p /mnt/host/lib
+    cp -p $LIB_DIR/*.jar /mnt/host/lib
+else
+    export GIT_BRANCH=origin/$(basename ${GITHUB_REF})
+    export GIT_COMMIT=${GITHUB_SHA}
+    export WORKSPACE=$GITHUB_WORKSPACE
+fi
