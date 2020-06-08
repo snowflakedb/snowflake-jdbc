7d11e9db2d347f86222ab58e85bc28abaf0bd71d
Shige Takeda <shige.takeda@snowflake.com>
2020-06-08 19:11:21 +0000

    skip keypair tests for FIPS against prod

---
+++ FIPS/src/test/java/net/snowflake/client/RunningOnTravisCI.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2012-2019 Snowflake Computing Inc. All right reserved.
+ */
+package net.snowflake.client;
+
+/**
+ * Run tests on CI
+ */
+public class RunningOnTravisCI implements ConditionalIgnoreRule.IgnoreCondition
+{
+  public boolean isSatisfied()
+  {
+    return System.getenv("GITHUB_ACTIONS") != null;
+  }
+
+  public static boolean isRunningOnTravisCI()
+  {
+    return System.getenv("GITHUB_ACTIONS") != null;
+  }
+}
