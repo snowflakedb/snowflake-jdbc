75d8eba774a769fec2c14220cddb8bbe670527f4
sfc-gh-mknister <meg.knister@snowflake.com>
2020-07-01 23:16:15 +0000

    added half test

--- src/test/java/net/snowflake/client/jdbc/telemetryOOB/TelemetryServiceIT.java
+++ src/test/java/net/snowflake/client/jdbc/telemetryOOB/TelemetryServiceIT.java
@@ -3,6 +3,7 @@ package net.snowflake.client.jdbc.telemetryOOB;
 
 import net.snowflake.client.jdbc.BaseJDBCTest;
 import net.snowflake.client.category.TestCategoryCore;
+import net.snowflake.client.jdbc.SnowflakeSQLLoggedException;
 import org.apache.commons.lang3.time.StopWatch;
 import org.junit.After;
 import org.junit.Before;
@@ -10,6 +11,8 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -230,5 +233,59 @@ public class TelemetryServiceIT extends BaseJDBCTest
       }
     }
     sw.stop();
+  }
+
+  /**
+   * Manual test case for checking telemetry message for SnowflakeSQLExceptions. Connect to dev or prod account and
+   * query client_telemetry table in s3testaccount for results.
+   * @throws SQLException
+   */
+  @Ignore
+  @Test
+  public void testSnowflakeSQLLoggedExceptionTelemetry() throws SQLException
+  {
+    // make a connection to initialize telemetry instance
+    Connection con = getConnection();
+    try {
+      generateDummyException(5);
+    }
+    catch (SnowflakeSQLLoggedException e)
+    {
+      e.printStackTrace();
+    }
+    /* Telemetry data should look like this when collected:
+    {
+      "Created_On": "2020-07-01 21:48:22",
+      "Enqueued_On": "2020-07-01 21:48:23",
+      "Name": "Exception: This is a test exception.",
+      "SchemaVersion": 1,
+      "Staged_On": "2020-07-01 21:48:44",
+      "Tags": {
+        "UUID": "f5e8ba4e-4136-4a32-9b69-c4824971a460",
+        "connectionString": "http://snowflake.dev.local:8080?ROLE=sysadmin&ACCOUNT=testaccount&PASSWORD=☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺",
+        "ctx_account": "testaccount",
+        "ctx_db": "testdb",
+        "ctx_insecuremode": "false",
+        "ctx_internal": "true",
+        "ctx_role": "sysadmin",
+        "ctx_schema": "testschema",
+        "ctx_ssl": "off",
+        "ctx_user": "snowman",
+        "ctx_warehouse": "regress",
+        "driver": "JDBC",
+        "snowhouseSchema": "dev",
+        "telemetryServerDeployment": "dev",
+        "version": "3.12.8"
+      },
+      "Type": "Log",
+      "UUID": "f5e8ba4e-4136-4a32-9b69-c4824971a460",
+      "Value": {
+        "Query ID": "01234567-1234-1234-1234-00001abcdefg",
+        "SQLState": "02000",
+        "Stacktrace": "net.snowflake.client.jdbc.SnowflakeSQLLoggedException: This is a test exception.\n\tat net.snowflake.client.jdbc.telemetryOOB.TelemetryServiceIT.generateDummyException(TelemetryServiceIT.java:246)\n\tat net.snowflake.client.jdbc.telemetryOOB.TelemetryServiceIT.testSnowflakeSQLLoggedExceptionTelemetry(TelemetryServiceIT.java:262)\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\n\tat java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\n\tat java.base/java.lang.reflect.Method.invoke(Method.java:567)\n\tat org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:50)\n\tat org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)\n\tat org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:47)\n\tat org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)\n\tat org.junit.internal.runners.statements.RunBefores.evaluate(RunBefores.java:26)\n\tat org.junit.internal.runners.statements.RunAfters.evaluate(RunAfters.java:27)\n\tat org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:325)\n\tat org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:78)\n\tat org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:57)\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\n\tat org.junit.runner.JUnitCore.run(JUnitCore.java:137)\n\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:68)\n\tat com.intellij.rt.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:33)\n\tat com.intellij.rt.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:230)\n\tat com.intellij.rt.junit.JUnitStarter.main(JUnitStarter.java:58)\n",
+        "Vendor Code": 0
+      }
+    }
+     */
   }
 }
