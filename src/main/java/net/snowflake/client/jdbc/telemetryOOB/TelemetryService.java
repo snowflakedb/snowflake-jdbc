a0bef9dfbe01a8024491de5068844183dbecc65e
wshangguan <wufan.shangguan@snowflake.com>
2020-07-21 09:29:42 +0000

    merge two debug info

--- src/main/java/net/snowflake/client/jdbc/telemetryOOB/TelemetryService.java
+++ src/main/java/net/snowflake/client/jdbc/telemetryOOB/TelemetryService.java
@@ -351,8 +351,7 @@ public class TelemetryService {
 
     private void uploadPayload()
     {
-      logger.debug("Running telemetry uploader");
-      logger.debug("Payload of telemetry is: " + payload);
+      logger.debug("Running telemetry uploader. The payload is: " + payload);
       CloseableHttpResponse response = null;
       boolean success = true;
 
