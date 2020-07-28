9dd5a9abe835883b11c0e0bee41856ace8138c12
Shige Takeda <shige.takeda@snowflake.com>
2020-07-28 20:27:24 +0000

    format

--- src/test/java/net/snowflake/client/jdbc/ResultSetJsonVsArrowMultiTZIT.java
+++ src/test/java/net/snowflake/client/jdbc/ResultSetJsonVsArrowMultiTZIT.java
@@ -10,7 +10,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
