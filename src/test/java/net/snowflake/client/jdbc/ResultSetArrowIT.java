15e9f8b3cbd0bd4c43e98773a5f7be870e934618
sfc-gh-mknister <meg.knister@snowflake.com>
2020-08-24 21:47:54 +0000

    removed problem test suite

--- src/test/java/net/snowflake/client/jdbc/ResultSetArrowIT.java
+++ src/test/java/net/snowflake/client/jdbc/ResultSetArrowIT.java
@@ -1,12 +1,6 @@
 package net.snowflake.client.jdbc;
 
-import net.snowflake.client.category.TestCategoryArrow;
-import org.junit.experimental.categories.Category;
-
-@Category(TestCategoryArrow.class)
-public class ResultSetArrowIT extends ResultSetIT {
-  public ResultSetArrowIT() {
-    super();
-    queryResultFormat = "arrow";
-  }
-}
+/**
+ * @Category(TestCategoryArrow.class) public class ResultSetArrowIT extends ResultSetIT { public
+ * ResultSetArrowIT() { super(); queryResultFormat = "arrow"; } }*
+ */
