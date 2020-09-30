bc5dffd36622d99566b7d495cc0363df399cfcba
Shige Takeda <shige.takeda@snowflake.com>
2020-09-30 20:00:42 +0000

    fixed merge conflict

--- src/test/java/net/snowflake/client/jdbc/ResultSetIT.java
+++ src/test/java/net/snowflake/client/jdbc/ResultSetIT.java
@@ -17,7 +17,6 @@ import java.util.Properties;
 import net.snowflake.client.ConditionalIgnoreRule;
 import net.snowflake.client.RunningOnGithubAction;
 import net.snowflake.client.category.TestCategoryResultSet;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
