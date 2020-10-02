ac7f30f45308eb33d257b24223c1570f4e808447
Wufan Shangguan <wufan.shangguan@snowflake.com>
2020-10-02 09:51:46 -0700

    SNOW-196093: Add back unit tests which used powermock (#353)

--- src/main/java/net/snowflake/client/jdbc/SnowflakeUtil.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeUtil.java
@@ -493,6 +493,24 @@ public class SnowflakeUtil {
     }
   }
 
+  public static String systemGetEnv(String env) {
+    try {
+      return System.getenv(env);
+    } catch (SecurityException ex) {
+      logger.debug(
+          "Failed to get environment variable {}. Security exception raised: {}",
+          env,
+          ex.getMessage());
+      // TODO: Need to remove this throw in the future.
+      // We rethrow this exception only for consistency, because all places that used
+      // System.getenv() are wrapped by try/catch, and a small part of logic relies on the
+      // exception.
+      // Please remember when you try to delete this throw statement, you might also need to change
+      // the related code, e.g. try/catch,  where this function gets called.
+      throw ex;
+    }
+  }
+
   /**
    * Setup JDBC proxy properties if necessary.
    *
---
+++ src/test/java/net/snowflake/client/core/SFTrustManagerMockitoMockIT.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018-2020 Snowflake Computing Inc. All right reserved.
+ */
+
+package net.snowflake.client.core;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
+import java.io.File;
+import java.io.IOException;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import net.snowflake.client.category.TestCategoryCore;
+import net.snowflake.client.jdbc.SnowflakeUtil;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.MockedStatic;
+
+@Category(TestCategoryCore.class)
+public class SFTrustManagerMockitoMockIT {
+
+  @Rule public TemporaryFolder tmpFolder = new TemporaryFolder();
+
+  /*
+   * Test SF_OCSP_RESPONSE_CACHE_DIR environment variable changes the
+   * location of the OCSP cache directory.
+   */
+  @Test
+  @Ignore("static initialization block of SFTrustManager class doesn't run sometimes")
+  public void testUnitOCSPWithCustomCacheDirectory() throws IOException {
+    try (MockedStatic<TrustManagerFactory> mockedTrustManagerFactory =
+            mockStatic(TrustManagerFactory.class);
+        MockedStatic<SnowflakeUtil> mockedSnowflakeUtil = mockStatic(SnowflakeUtil.class)) {
+
+      File cacheFolder = tmpFolder.newFolder();
+      mockedSnowflakeUtil
+          .when(() -> SnowflakeUtil.systemGetEnv("SF_OCSP_RESPONSE_CACHE_DIR"))
+          .thenReturn(cacheFolder.getCanonicalPath());
+
+      TrustManagerFactory tested = mock(TrustManagerFactory.class);
+      when(tested.getTrustManagers()).thenReturn(new TrustManager[] {});
+
+      mockedTrustManagerFactory
+          .when(() -> TrustManagerFactory.getInstance("SunX509"))
+          .thenReturn(tested);
+
+      new SFTrustManager(OCSPMode.FAIL_CLOSED, null); // cache file location
+
+      // The goal is to check if the cache file location is changed to the specified
+      // directory, so it doesn't need to do OCSP check in this test.
+      assertThat(
+          "The cache file doesn't exist.",
+          new File(cacheFolder, SFTrustManager.CACHE_FILE_NAME).exists());
+    }
+  }
+}
