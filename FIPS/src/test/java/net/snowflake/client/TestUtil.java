91abf079e1ff6dcb0869cdce9237c746cee9c864
Shige Takeda <shige.takeda@snowflake.com>
2020-10-02 19:39:36 +0000

    fixed FIPS

---
+++ FIPS/src/test/java/net/snowflake/client/TestUtil.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2012-2019 Snowflake Computing Inc. All right reserved.
+ */
+package net.snowflake.client;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import net.snowflake.client.core.SFException;
+import net.snowflake.client.log.SFLogger;
+import net.snowflake.client.log.SFLoggerFactory;
+import org.junit.Assert;
+
+public class TestUtil {
+  private static final SFLogger logger = SFLoggerFactory.getLogger(TestUtil.class);
+  /**
+   * Util function to assert a piece will throw exception and assert on the error code
+   *
+   * @param errorCode expected error code
+   * @param testCode the code that will run and throws exception
+   */
+  public static void assertSFException(int errorCode, TestRunInterface testCode) {
+    try {
+      testCode.run();
+      Assert.fail();
+    } catch (SFException e) {
+      assertThat(e.getVendorCode(), is(errorCode));
+    }
+  }
+
+  /** Functional interface used to run a piece of code which throws SFException */
+  @FunctionalInterface
+  public interface TestRunInterface {
+    void run() throws SFException;
+  }
+
+  /**
+   * System.getenv wrapper. If System.getenv raises an SecurityException, it is ignored and returns
+   * null.
+   *
+   * <p>This is replicated from SnowflakeUtil.systemGetEnv, because the old driver doesn't have that
+   * function for the tests to use it. Replace this function call with SnowflakeUtil.systemGetEnv
+   * when it is available.
+   *
+   * @param env the environment variable name.
+   * @return the environment variable value if set, otherwise null.
+   */
+  public static String systemGetEnv(String env) {
+    try {
+      return System.getenv(env);
+    } catch (SecurityException ex) {
+      logger.debug(
+          "Failed to get environment variable {}. Security exception raised: {}",
+          env,
+          ex.getMessage());
+    }
+    return null;
+  }
+}
