1e26e3cdcd480cd6f9a07c400bb3274657edc164
Shige Takeda <shige.takeda@snowflake.com>
2020-10-04 03:13:44 +0000

    rebase and split the tests for old driver

--- src/test/java/net/snowflake/client/jdbc/ResultSetMultiTimeZoneIT.java
+++ src/test/java/net/snowflake/client/jdbc/ResultSetMultiTimeZoneIT.java
@@ -46,8 +46,8 @@ public class ResultSetMultiTimeZoneIT extends BaseJDBCTest {
     System.setProperty("user.timezone", timeZone);
   }
 
-  public static Connection init(int injectSocketTimeout) throws SQLException {
-    Connection connection = BaseJDBCTest.getConnection(injectSocketTimeout);
+  public Connection init() throws SQLException {
+    Connection connection = BaseJDBCTest.getConnection();
 
     Statement statement = connection.createStatement();
     statement.execute(
@@ -59,15 +59,10 @@ public class ResultSetMultiTimeZoneIT extends BaseJDBCTest {
             + "TIMESTAMP_LTZ_OUTPUT_FORMAT='DY, DD MON YYYY HH24:MI:SS TZHTZM',"
             + "TIMESTAMP_NTZ_OUTPUT_FORMAT='DY, DD MON YYYY HH24:MI:SS TZHTZM'");
     statement.close();
+    connection
+        .createStatement()
+        .execute("alter session set jdbc_query_result_format = '" + queryResultFormat + "'");
     return connection;
-  }
-
-  public Connection init() throws SQLException {
-    Connection conn = init(BaseJDBCTest.DONT_INJECT_SOCKET_TIMEOUT);
-    Statement stmt = conn.createStatement();
-    stmt.execute("alter session set jdbc_query_result_format = '" + queryResultFormat + "'");
-    stmt.close();
-    return conn;
   }
 
   public Connection init(Properties paramProperties) throws SQLException {
@@ -174,66 +169,6 @@ public class ResultSetMultiTimeZoneIT extends BaseJDBCTest {
     assertEquals(date, resultSet.getDate(1));
     assertEquals(date, resultSet.getDate("COLA"));
     statement.execute("drop table if exists datetime");
-    connection.close();
-  }
-
-  /**
-   * Test for getDate(int columnIndex, Calendar cal) function to ensure it matches values with
-   * getTimestamp function
-   */
-  @Test
-  @ConditionalIgnoreRule.ConditionalIgnore(condition = RunningOnGithubAction.class)
-  public void testDateAndTimestampWithTimezone() throws SQLException {
-    Connection connection = init();
-    Statement statement = connection.createStatement();
-    statement.execute("alter session set JDBC_FORMAT_DATE_WITH_TIMEZONE=true");
-    ResultSet rs =
-        statement.executeQuery(
-            "SELECT DATE '1970-01-02 00:00:00' as datefield, "
-                + "TIMESTAMP '1970-01-02 00:00:00' as timestampfield");
-    rs.next();
-
-    // Set a timezone for results to be returned in and set a format for date and timestamp objects
-    Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    sdf.setTimeZone(cal.getTimeZone());
-
-    // Date object and calendar object should return the same timezone offset with calendar
-    Date dateWithZone = rs.getDate(1, cal);
-    Timestamp timestampWithZone = rs.getTimestamp(2, cal);
-    assertEquals(sdf.format(dateWithZone), sdf.format(timestampWithZone));
-
-    // When fetching Date object with getTimestamp versus Timestamp object with getTimestamp,
-    // results should match
-    assertEquals(rs.getTimestamp(1, cal), rs.getTimestamp(2, cal));
-
-    // When fetching Timestamp object with getDate versus Date object with getDate, results should
-    // match
-    assertEquals(rs.getDate(1, cal), rs.getDate(2, cal));
-
-    // getDate() without Calendar offset called on Date type should return the same date with no
-    // timezone offset
-    assertEquals("1970-01-02 00:00:00", sdf.format(rs.getDate(1)));
-    // getDate() without Calendar offset called on Timestamp type returns date with timezone offset
-    assertEquals("1970-01-02 08:00:00", sdf.format(rs.getDate(2)));
-
-    // getTimestamp() without Calendar offset called on Timestamp type should return the timezone
-    // offset
-    assertEquals("1970-01-02 08:00:00", sdf.format(rs.getTimestamp(2)));
-    // getTimestamp() without Calendar offset called on Date type should not return the timezone
-    // offset
-    assertEquals("1970-01-02 00:00:00", sdf.format(rs.getTimestamp(1)));
-
-    // test that session parameter functions as expected. When false, getDate() has same behavior
-    // with or without Calendar input
-    statement.execute("alter session set JDBC_FORMAT_DATE_WITH_TIMEZONE=false");
-    rs = statement.executeQuery("SELECT DATE '1945-05-10 00:00:00' as datefield");
-    rs.next();
-    assertEquals(rs.getDate(1, cal), rs.getDate(1));
-    assertEquals("1945-05-10 00:00:00", sdf.format(rs.getDate(1, cal)));
-
-    rs.close();
-    statement.close();
     connection.close();
   }
 
