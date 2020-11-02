b71a511666ff92d7510f4fcb71789dcc04fe6e25
sfc-gh-mknister <66097847+sfc-gh-mknister@users.noreply.github.com>
2020-11-02 12:45:07 -0800

    SNOW-210997 Cover code that wasn't covered in PRs from last 2 months (#384)

--- src/main/java/net/snowflake/client/log/JDK14Logger.java
+++ src/main/java/net/snowflake/client/log/JDK14Logger.java
@@ -10,11 +10,7 @@ import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+import java.util.logging.*;
 import net.snowflake.client.core.EventHandler;
 import net.snowflake.client.core.EventUtil;
 import net.snowflake.client.util.SecretDetector;
@@ -154,9 +150,19 @@ public class JDK14Logger implements SFLogger {
     snowflakeLogger.addHandler(handler);
   }
 
+  public static void removeHandler(Handler handler) {
+    Logger snowflakeLogger = Logger.getLogger(SFFormatter.CLASS_NAME_PREFIX);
+    snowflakeLogger.removeHandler(handler);
+  }
+
   public static void setLevel(Level level) {
     Logger snowflakeLogger = Logger.getLogger(SFFormatter.CLASS_NAME_PREFIX);
     snowflakeLogger.setLevel(level);
+  }
+
+  public static Level getLevel() {
+    Logger snowflakeLogger = Logger.getLogger(SFFormatter.CLASS_NAME_PREFIX);
+    return snowflakeLogger.getLevel();
   }
 
   /**
---
+++ src/test/java/net/snowflake/client/log/SLF4JJJCLWrapperLatestIT.java
@@ -0,0 +1,131 @@
+package net.snowflake.client.log;
+
+import static org.junit.Assert.*;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.AppenderBase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SLF4JJJCLWrapperLatestIT {
+
+  /** Message last logged using SLF4JLogger. */
+  private String lastLogMessage = null;
+
+  /** Level of logging to restore at the end of testing. Most likely null. */
+  private Level levelToRestore = null;
+
+  /** An appender that will be used for getting messages logged by a {@link Logger} instance */
+  private class TestAppender extends AppenderBase<ILoggingEvent> {
+    @Override
+    public void append(ILoggingEvent event) {
+      // Assign the log message and it's level to the outer class instance
+      // variables so that it can see the messages logged
+      lastLogMessage = event.getFormattedMessage();
+    }
+  }
+
+  String getLoggedMessage() {
+    return this.lastLogMessage;
+  }
+
+  /** Logging levels */
+  private enum LogLevel {
+    ERROR,
+    WARN,
+    INFO,
+    DEBUG,
+    TRACE
+  }
+
+  SLF4JJCLWrapper wrapper = new SLF4JJCLWrapper(SLF4JJJCLWrapperLatestIT.class.getName());
+  Logger logger = (Logger) wrapper.getLogger();
+  private final Appender<ILoggingEvent> testAppender = new TestAppender();
+
+  @Before
+  public void setUp() {
+    levelToRestore = logger.getLevel();
+    if (!testAppender.isStarted()) {
+      testAppender.start();
+    }
+    // Set debug level to lowest possible level so all messages can be recorded.
+    logger.setLevel(Level.TRACE);
+    logger.addAppender(testAppender);
+  }
+
+  @After
+  public void tearDown() {
+    logger.setLevel(levelToRestore);
+    logger.detachAppender(testAppender);
+  }
+
+  // helper function, throwables allowed
+  private void testNullLogMessagesWithThrowable(LogLevel level, String message, Throwable t) {
+    switch (level) {
+      case ERROR:
+        wrapper.error(message, t);
+        break;
+      case WARN:
+        wrapper.warn(message, t);
+        break;
+      case INFO:
+        wrapper.info(message, t);
+        break;
+      case DEBUG:
+        wrapper.debug(message, t);
+        break;
+      case TRACE:
+        wrapper.trace(message, t);
+        break;
+    }
+    assertEquals(null, getLoggedMessage());
+  }
+
+  // helper function, no throwables
+  private void testNullLogMessagesNoThrowable(LogLevel level, String message) {
+    switch (level) {
+      case ERROR:
+        wrapper.error(message);
+        break;
+      case WARN:
+        wrapper.warn(message);
+        break;
+      case INFO:
+        wrapper.info(message);
+        break;
+      case DEBUG:
+        wrapper.debug(message);
+        break;
+      case TRACE:
+        wrapper.trace(message);
+        break;
+    }
+    assertEquals(null, getLoggedMessage());
+  }
+
+  /** Test that all levels are disabled for wrapper class. No messages returned at any level. */
+  @Test
+  public void testNullLogMessages() {
+    for (LogLevel level : LogLevel.values()) {
+      testNullLogMessagesWithThrowable(level, "sample message", null);
+      testNullLogMessagesNoThrowable(level, "sample message");
+    }
+  }
+
+  /**
+   * Test that tracing and debugging are disabled for apache at all times. With other levels, pass
+   * in results from the actual logger to see if messages can be enabled.
+   */
+  @Test
+  public void testEnabledMessaging() {
+    assertFalse(wrapper.isTraceEnabled());
+    assertFalse(wrapper.isDebugEnabled());
+    assertTrue(wrapper.isInfoEnabled());
+    assertTrue(wrapper.isErrorEnabled());
+    assertTrue(wrapper.isFatalEnabled());
+  }
+}
