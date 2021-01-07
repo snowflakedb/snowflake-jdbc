c9af540dc0fa90157009120ff1d09a04bf4c5045
sfc-gh-mknister <66097847+sfc-gh-mknister@users.noreply.github.com>
2021-01-06 18:49:56 -0800

    SNOW-174428 Change bulk array binds to be uploaded with streaming PUT instead of local file creation + PUT (#418)
    * SNOW-174428 bulk array binds uploaded with streaming instead of file creation

--- src/test/java/net/snowflake/client/jdbc/BindUploaderLatestIT.java
+++ src/test/java/net/snowflake/client/jdbc/BindUploaderLatestIT.java
@@ -4,23 +4,17 @@
 package net.snowflake.client.jdbc;
 
 import static net.snowflake.client.jdbc.BindUploaderIT.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
+import java.util.Map;
 import java.util.TimeZone;
-import java.util.zip.GZIPInputStream;
 import net.snowflake.client.category.TestCategoryOthers;
+import net.snowflake.client.core.ParameterBindingDTO;
 import net.snowflake.client.core.SFSession;
 import net.snowflake.client.core.bind.BindUploader;
 import org.junit.*;
@@ -65,59 +59,28 @@ public class BindUploaderLatestIT extends BaseJDBCTest {
     TimeZone.setDefault(prevTimeZone);
   }
 
-  // Test multiple csv creation on reaching size limit
-  @Test
-  public void testSerializeCSVMultiple() throws Exception {
-    // after each write, the size will exceed 1 byte, so there should be 2 files
-    bindUploader.setFileSize(1);
-
-    bindUploader.upload(getBindings(conn));
-
-    // CSV should exist in bind directory until the uploader is closed
-    Path p = bindUploader.getBindDir();
-    assertTrue(Files.exists(p));
-    assertTrue(Files.isDirectory(p));
-
-    File[] files = p.toFile().listFiles();
-    Arrays.sort(files);
-    assertNotNull(files);
-    assertEquals(2, files.length);
-    File csvFile = files[0];
-    try (BufferedReader br =
-        new BufferedReader(
-            new InputStreamReader(new GZIPInputStream(new FileInputStream(csvFile))))) {
-      assertEquals(csv1, br.readLine());
-      assertNull(br.readLine());
-    }
-
-    csvFile = files[1];
-    try (BufferedReader br =
-        new BufferedReader(
-            new InputStreamReader(new GZIPInputStream(new FileInputStream(csvFile))))) {
-      assertEquals(csv2, br.readLine());
-      assertNull(br.readLine());
-    }
-
-    bindUploader.close();
-
-    // After the uploader closes, it should clean up the CSV
-    assertFalse(Files.exists(p));
-  }
-  // Test multiple csv upload and successful parsing
+  // Test setting the input stream buffer size to be quite small and ensure that 2 files (1
+  // corresponding to each input stream) are created in internal stage.
   @Test
   public void testUploadedResultsMultiple() throws Exception {
-    // after each write, the size will exceed 1 byte, so there should be 2 files
-    bindUploader.setFileSize(1);
 
+    // Get an estimate of how many bytes are in 1 of the rows being uploaded and set this value to
+    // be the input stream buffer size. For 2 similarly sized rows, we should now have 2 files.
+    int lengthOfOneRow = csv1.getBytes("UTF-8").length;
+    bindUploader.setInputStreamBufferSize(lengthOfOneRow);
     bindUploader.upload(getBindings(conn));
-
+    // assert 2 files were created on internal stage
+    assertEquals(2, bindUploader.getFileCount());
     Statement stmt = conn.createStatement();
+    // assert that the results look proper
     ResultSet rs = stmt.executeQuery(SELECT_FROM_STAGE);
     rs.next();
     assertEquals(csv1, parseRow(rs));
     rs.next();
     assertEquals(csv2, parseRow(rs));
     assertFalse(rs.next());
+    rs.close();
+    stmt.close();
   }
 
   // Test single csv upload and successful parsing
@@ -132,34 +95,29 @@ public class BindUploaderLatestIT extends BaseJDBCTest {
     rs.next();
     assertEquals(csv2, parseRow(rs));
     assertFalse(rs.next());
+    rs.close();
+    stmt.close();
   }
 
-  // Test csv correctness, and deletion after close
   @Test
-  public void testSerializeCSVSimple() throws Exception {
-    bindUploader.upload(getBindings(conn));
-
-    // CSV should exist in bind directory until the uploader is closed
-    Path p = bindUploader.getBindDir();
-    assertTrue(Files.exists(p));
-    assertTrue(Files.isDirectory(p));
-
-    File[] files = p.toFile().listFiles();
-    assertNotNull("file must exists", files);
-    assertEquals(files.length, 1);
-
-    File csvFile = files[0];
-    try (BufferedReader br =
-        new BufferedReader(
-            new InputStreamReader(new GZIPInputStream(new FileInputStream(csvFile))))) {
-      assertEquals(csv1, br.readLine());
-      assertEquals(csv2, br.readLine());
-      assertNull(br.readLine());
+  public void testUploadStreamLargeBatch() throws Exception {
+    // create large batch so total bytes transferred are about 10 times the size of input stream
+    // buffer
+    int batchSize = 1024 * 1024;
+    SnowflakePreparedStatementV1 stmt =
+        (SnowflakePreparedStatementV1) conn.prepareStatement(dummyInsert);
+    for (int i = 0; i < batchSize; i++) {
+      bind(stmt, row1);
     }
-
-    bindUploader.close();
-
-    // After the uploader closes, it should clean up the CSV
-    assertFalse(Files.exists(p));
+    Map<String, ParameterBindingDTO> parameterBindings = stmt.getBatchParameterBindings();
+    bindUploader.upload(parameterBindings);
+    ResultSet rs = stmt.executeQuery(SELECT_FROM_STAGE);
+    for (int i = 0; i < batchSize; i++) {
+      rs.next();
+      assertEquals(csv1, parseRow(rs));
+    }
+    assertFalse(rs.next());
+    rs.close();
+    stmt.close();
   }
 }
