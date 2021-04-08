bdd6a36274c3cbbaa91cfebdd1c33ba5ff188947
viswa <66025379+sfc-gh-vreddy@users.noreply.github.com>
2021-04-08 11:28:13 -0700

    Revert "use s3 regional url when parameter is set" (#479)

--- src/main/java/net/snowflake/client/core/SessionUtil.java
+++ src/main/java/net/snowflake/client/core/SessionUtil.java
@@ -103,8 +103,6 @@ public class SessionUtil {
       "CLIENT_METADATA_REQUEST_USE_CONNECTION_CTX";
   public static final String CLIENT_METADATA_USE_SESSION_DATABASE =
       "CLIENT_METADATA_USE_SESSION_DATABASE";
-  public static final String FORCE_REGIONAL_S3_ENDPOINTS_FOR_PRESIGNED_URLS =
-      "FORCE_REGIONAL_S3_ENDPOINTS_FOR_PRESIGNED_URLS";
 
   static final String SF_HEADER_SERVICE_NAME = "X-Snowflake-Service";
 
@@ -177,8 +175,7 @@ public class SessionUtil {
               "JDBC_TREAT_DECIMAL_AS_INT",
               "JDBC_ENABLE_COMBINED_DESCRIBE",
               CLIENT_ENABLE_CONSERVATIVE_MEMORY_USAGE,
-              CLIENT_VALIDATE_DEFAULT_PARAMETERS,
-              FORCE_REGIONAL_S3_ENDPOINTS_FOR_PRESIGNED_URLS));
+              CLIENT_VALIDATE_DEFAULT_PARAMETERS));
 
   /**
    * Returns Authenticator type
@@ -1383,15 +1380,9 @@ public class SessionUtil {
         if (session != null) {
           session.setValidateDefaultParameters(SFLoginInput.getBooleanValue(entry.getValue()));
         }
-      } else if (FORCE_REGIONAL_S3_ENDPOINTS_FOR_PRESIGNED_URLS.equalsIgnoreCase(
-          (entry.getKey()))) {
+      } else {
         if (session != null) {
-          session.setUseRegionalS3EndpointsForPresignedURL(
-              SFLoginInput.getBooleanValue(entry.getValue()));
-        } else {
-          if (session != null) {
-            session.setOtherParameter(entry.getKey(), entry.getValue());
-          }
+          session.setOtherParameter(entry.getKey(), entry.getValue());
         }
       }
     }
