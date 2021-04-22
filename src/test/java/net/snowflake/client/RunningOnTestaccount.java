984c7783f02a4c676c1b886bc302e3f6498f4817
sfc-gh-mknister <meg.knister@snowflake.com>
2021-04-22 04:58:03 +0000

    try again, fix ignore logic

--- src/test/java/net/snowflake/client/RunningOnTestaccount.java
+++ src/test/java/net/snowflake/client/RunningOnTestaccount.java
@@ -2,6 +2,6 @@ package net.snowflake.client;
 
 public class RunningOnTestaccount implements ConditionalIgnoreRule.IgnoreCondition {
   public boolean isSatisfied() {
-    return "testaccount".equals(TestUtil.systemGetEnv("SNOWFLAKE_TEST_ACCOUNT"));
+    return TestUtil.systemGetEnv("SNOWFLAKE_TEST_ACCOUNT").contains("testaccount");
   }
 }
