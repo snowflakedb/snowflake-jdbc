92121767aeda1b3435e0ec936e7fda8cd4077272
sfc-gh-mnaides <matthew.naides@snowflake.com>
2021-04-27 23:25:19 +0000

    support unencrypted sessionless upload

--- src/test/java/net/snowflake/client/jdbc/FileUploaderSessionlessTest.java
+++ src/test/java/net/snowflake/client/jdbc/FileUploaderSessionlessTest.java
@@ -197,6 +197,26 @@ public class FileUploaderSessionlessTest {
   }
 
   @Test
+  public void testGetEncryptionMaterialMissing() throws Exception {
+    JsonNode modifiedNode = exampleS3JsonNode.deepCopy();
+    ObjectNode foo = (ObjectNode) modifiedNode.path("data");
+    foo.remove("encryptionMaterial");
+
+    List<RemoteStoreFileEncryptionMaterial> expected = new ArrayList<>();
+    RemoteStoreFileEncryptionMaterial content =
+        new RemoteStoreFileEncryptionMaterial(
+            "EXAMPLE_QUERY_STAGE_MASTER_KEY", "EXAMPLE_QUERY_ID", 123L);
+    expected.add(content);
+
+    List<RemoteStoreFileEncryptionMaterial> encryptionMaterials =
+        SnowflakeFileTransferAgent.getEncryptionMaterial(
+            SFBaseFileTransferAgent.CommandType.UPLOAD, modifiedNode);
+
+    Assert.assertEquals(1, encryptionMaterials.size());
+    Assert.assertNull(encryptionMaterials.get(0));
+  }
+
+  @Test
   public void testGetEncryptionMaterial() throws Exception {
     List<RemoteStoreFileEncryptionMaterial> expected = new ArrayList<>();
     RemoteStoreFileEncryptionMaterial content =
@@ -299,6 +319,48 @@ public class FileUploaderSessionlessTest {
         "EXAMPLE_QUERY_STAGE_MASTER_KEY",
         metadata.getEncryptionMaterial().getQueryStageMasterKey());
     Assert.assertEquals(123L, (long) metadata.getEncryptionMaterial().getSmkId());
+
+    // Misc check
+    Assert.assertEquals(SFBaseFileTransferAgent.CommandType.UPLOAD, metadata.getCommandType());
+    Assert.assertNull(metadata.getPresignedUrl());
+    Assert.assertEquals("orders_100.csv", metadata.getPresignedUrlFileName());
+  }
+
+  @Test
+  public void testGetFileTransferMetadatasS3MissingEncryption() throws Exception {
+    JsonNode modifiedNode = exampleS3JsonNode.deepCopy();
+    ObjectNode foo = (ObjectNode) modifiedNode.path("data");
+    foo.remove("encryptionMaterial");
+
+    List<SnowflakeFileTransferMetadata> metadataList =
+        SnowflakeFileTransferAgent.getFileTransferMetadatas(modifiedNode);
+    Assert.assertEquals(1, metadataList.size());
+
+    SnowflakeFileTransferMetadataV1 metadata =
+        (SnowflakeFileTransferMetadataV1) metadataList.get(0);
+
+    // StageInfo check
+    StageInfo stageInfo = metadata.getStageInfo();
+
+    Map<String, String> expectedCreds = new HashMap<>();
+    expectedCreds.put("AWS_ID", "EXAMPLE_AWS_ID");
+    expectedCreds.put("AWS_KEY", "EXAMPLE_AWS_KEY");
+    expectedCreds.put("AWS_KEY_ID", "EXAMPLE_AWS_KEY_ID");
+    expectedCreds.put("AWS_SECRET_KEY", "EXAMPLE_AWS_SECRET_KEY");
+    expectedCreds.put("AWS_TOKEN", "EXAMPLE_AWS_TOKEN");
+
+    Assert.assertEquals(StageInfo.StageType.S3, stageInfo.getStageType());
+    Assert.assertEquals("stage/location/foo/", stageInfo.getLocation());
+    Assert.assertEquals(expectedCreds, stageInfo.getCredentials());
+    Assert.assertEquals("us-west-2", stageInfo.getRegion());
+    Assert.assertEquals(null, stageInfo.getEndPoint());
+    Assert.assertEquals(null, stageInfo.getStorageAccount());
+    Assert.assertEquals(true, stageInfo.getIsClientSideEncrypted());
+
+    // EncryptionMaterial check
+    Assert.assertNull(metadata.getEncryptionMaterial().getQueryId());
+    Assert.assertNull(metadata.getEncryptionMaterial().getQueryStageMasterKey());
+    Assert.assertNull(metadata.getEncryptionMaterial().getSmkId());
 
     // Misc check
     Assert.assertEquals(SFBaseFileTransferAgent.CommandType.UPLOAD, metadata.getCommandType());
