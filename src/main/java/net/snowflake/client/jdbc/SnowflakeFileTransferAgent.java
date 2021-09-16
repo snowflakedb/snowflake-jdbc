64e36b888bfb5a156996e9fb5333618f83bc81bb
wshangguan <wufan.shangguan@snowflake.com>
2021-09-16 07:58:28 +0000

    add error handling

--- src/main/java/net/snowflake/client/jdbc/SnowflakeFileTransferAgent.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeFileTransferAgent.java
@@ -4,8 +4,6 @@
 
 package net.snowflake.client.jdbc;
 
-import static net.snowflake.client.jdbc.SnowflakeUtil.systemGetProperty;
-
 import com.amazonaws.util.Base64;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -14,19 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
 import com.google.common.io.ByteStreams;
 import com.google.common.io.CountingOutputStream;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.DigestOutputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.zip.GZIPOutputStream;
 import net.snowflake.client.core.*;
 import net.snowflake.client.jdbc.cloud.storage.*;
 import net.snowflake.client.jdbc.telemetryOOB.TelemetryService;
@@ -41,6 +26,22 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.GZIPOutputStream;
+
+import static net.snowflake.client.jdbc.SnowflakeUtil.systemGetProperty;
 
 /**
  * Class for uploading/downloading files
@@ -2324,7 +2325,26 @@ public class SnowflakeFileTransferAgent extends SFBaseFileTransferAgent {
 
       logger.debug("received object summaries from remote storage");
 
-      compareAndSkipRemoteFiles(objectSummaries, destFileNameToSrcFileMap);
+      retryCount = 0;
+      do {
+        try {
+          compareAndSkipRemoteFiles(objectSummaries, destFileNameToSrcFileMap);
+          break;
+        } catch (Exception ex) {
+          // This retry logic is mainly for Azure iterator. Since Azure iterator is a lazy iterator,
+          // it can throw exceptions during the for-each calls. To be more specific, iterator apis,
+          // e.g. hasNext(), may throw Storage service error.
+          logger.debug(
+              "Comparison with existing files in remote storage encountered exception.", ex);
+          if (ex instanceof StorageProviderException) {
+            ex =
+                (Exception)
+                    ex.getCause(); // Cause of StorageProviderException is always an Exception
+          }
+          storageClient.handleStorageException(
+              ex, ++retryCount, "compareRemoteFiles", session, command);
+        }
+      } while (retryCount <= storageClient.getMaxRetries());
     } else if (stageInfo.getStageType() == StageInfo.StageType.LOCAL_FS) {
       for (String stageFileName : stageFileNames) {
         String stageFilePath =
