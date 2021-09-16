5db8b6a551414a4c86e028c36aa5c925197e9d82
sfc-gh-mknister <meg.knister@snowflake.com>
2021-09-16 17:35:44 +0000

    more debugging log lines

--- src/main/java/net/snowflake/client/jdbc/SnowflakeChunkDownloader.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeChunkDownloader.java
@@ -4,11 +4,18 @@
 
 package net.snowflake.client.jdbc;
 
-import static net.snowflake.client.core.Constants.MB;
-
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.MappingJsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.snowflake.client.core.*;
+import net.snowflake.client.jdbc.SnowflakeResultChunk.DownloadState;
+import net.snowflake.client.jdbc.telemetryOOB.TelemetryService;
+import net.snowflake.client.log.ArgSupplier;
+import net.snowflake.client.log.SFLogger;
+import net.snowflake.client.log.SFLoggerFactory;
+import net.snowflake.common.core.SqlState;
+import org.apache.arrow.memory.RootAllocator;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -18,14 +25,8 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
-import net.snowflake.client.core.*;
-import net.snowflake.client.jdbc.SnowflakeResultChunk.DownloadState;
-import net.snowflake.client.jdbc.telemetryOOB.TelemetryService;
-import net.snowflake.client.log.ArgSupplier;
-import net.snowflake.client.log.SFLogger;
-import net.snowflake.client.log.SFLoggerFactory;
-import net.snowflake.common.core.SqlState;
-import org.apache.arrow.memory.RootAllocator;
+
+import static net.snowflake.client.core.Constants.MB;
 
 /**
  * Class for managing async download of offline result chunks
@@ -502,6 +503,7 @@ public class SnowflakeChunkDownloader implements ChunkDownloader {
     }
 
     SnowflakeResultChunk currentChunk = this.chunks.get(nextChunkToConsume);
+    logger.debug("getNextChunkToConsume() current chunk state: {}", currentChunk.getDownloadState());
 
     if (currentChunk.getDownloadState() == DownloadState.SUCCESS) {
       logger.debug("chunk #{} is ready to consume", nextChunkToConsume);
@@ -512,6 +514,7 @@ public class SnowflakeChunkDownloader implements ChunkDownloader {
       }
       return currentChunk;
     } else {
+      logger.debug("getNextChunkToConsume() has entered else block");
       // the chunk we want to consume is not ready yet, wait for it
       currentChunk.getLock().lock();
       try {
