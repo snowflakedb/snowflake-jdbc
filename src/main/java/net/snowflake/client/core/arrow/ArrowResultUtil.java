0ad26a0d383b67ad96a2c8dd9fd991a843efd045
sfc-gh-mknister <66097847+sfc-gh-mknister@users.noreply.github.com>
2021-09-20 14:02:08 -0700

    SNOW-366563 Fix London/Europe daylight savings offset with timestamp_ntz (#581)
    * SNOW-366463

--- src/main/java/net/snowflake/client/core/arrow/ArrowResultUtil.java
+++ src/main/java/net/snowflake/client/core/arrow/ArrowResultUtil.java
@@ -102,7 +102,7 @@ public class ArrowResultUtil {
    */
   private static long moveToTimeZoneOffset(
       long milliSecsSinceEpoch, TimeZone oldTZ, TimeZone newTZ) {
-    if (oldTZ.getRawOffset() == newTZ.getRawOffset()) {
+    if (oldTZ.hasSameRules(newTZ)) {
       // same time zone
       return 0;
     }
