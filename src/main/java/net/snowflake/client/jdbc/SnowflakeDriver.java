09fb00ebb99e3a133f2dcb5fbe243b836f982b14
Ankit Bhatnagar <ankit.bhatnagar@snowflake.com>
2021-10-12 06:50:53 -0700

    Version bump change

--- src/main/java/net/snowflake/client/jdbc/SnowflakeDriver.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeDriver.java
@@ -23,7 +23,7 @@ public class SnowflakeDriver implements Driver {
   static SnowflakeDriver INSTANCE;
 
   public static final Properties EMPTY_PROPERTIES = new Properties();
-  public static String implementVersion = "3.13.8";
+  public static String implementVersion = "3.13.9";
 
   static int majorVersion = 0;
   static int minorVersion = 0;
