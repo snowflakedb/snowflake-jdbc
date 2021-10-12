bfa96278457e89c53e1e307f28b91be9086b9c3e
Mingli Rui <mingli.rui@snowflake.com>
2021-10-12 13:12:48 -0700

    Print error message when testGetChildQueryIdsNegativeTestQueryIsRunning() fail.

--- src/test/java/net/snowflake/client/jdbc/ConnectionLatestIT.java
+++ src/test/java/net/snowflake/client/jdbc/ConnectionLatestIT.java
@@ -851,10 +851,14 @@ public class ConnectionLatestIT extends BaseJDBCTest {
       connection.unwrap(SnowflakeConnectionV1.class).getChildQueryIds(queryID);
       fail("The getChildQueryIds() should fail because query is running");
     } catch (SQLException ex) {
-      assertTrue(
-          ex.getMessage()
-              .contains(
-                  "Status of query associated with resultSet is RUNNING. Results not generated."));
+      String msg = ex.getMessage();
+      if (!msg.contains("Status of query associated with resultSet is")
+          || !msg.contains("Results not generated.")) {
+        ex.printStackTrace();
+        QueryStatus qs =
+            connection.unwrap(SnowflakeConnectionV1.class).getSfSession().getQueryStatus(queryID);
+        fail("Don't get expected message, query Status: " + qs + " actual message is: " + msg);
+      }
     } finally {
       connection.createStatement().execute("select system$cancel_query('" + queryID + "')");
       statement.close();
