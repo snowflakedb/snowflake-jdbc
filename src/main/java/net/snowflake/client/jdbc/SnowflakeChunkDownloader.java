8bcf60f6fd30e0c48b7d43c52f107856a9b342bd
Zihan Li <63482288+sfc-gh-zli@users.noreply.github.com>
2021-10-22 12:07:57 -0700

    SNOW-463141 make chunk downloader max retry configurable with a server side parameter (#610)

--- src/main/java/net/snowflake/client/jdbc/SnowflakeChunkDownloader.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeChunkDownloader.java
@@ -122,6 +122,10 @@ public class SnowflakeChunkDownloader implements ChunkDownloader {
   private static final int MAX_NUM_OF_RETRY = 10;
   private static final int MAX_RETRY_JITTER = 1000; // milliseconds
 
+  // Only controls the max retry number when prefetch runs out of memory
+  // Default value is MAX_NUM_OF_RETRY, which is 10
+  private int prefetchMaxRetry = MAX_NUM_OF_RETRY;
+
   private static Throwable injectedDownloaderException = null; // for testing purpose
 
   // This function should only be used for testing purpose
@@ -195,6 +199,13 @@ public class SnowflakeChunkDownloader implements ChunkDownloader {
         (resultSetSerializable.getSession() != null)
             ? resultSetSerializable.getSession().orElse(null)
             : null;
+    if (this.session != null) {
+      Object prefetchMaxRetry =
+          this.session.getOtherParameter(SessionUtil.JDBC_CHUNK_DOWNLOADER_MAX_RETRY);
+      if (prefetchMaxRetry != null) {
+        this.prefetchMaxRetry = (int) prefetchMaxRetry;
+      }
+    }
     this.memoryLimit = resultSetSerializable.getMemoryLimit();
     if (this.session != null
         && session.getMemoryLimitForTesting() != SFBaseSession.MEMORY_LIMIT_UNSET) {
@@ -374,7 +385,7 @@ public class SnowflakeChunkDownloader implements ChunkDownloader {
         // cancel the reserved memory
         logger.debug("cancel the reserved memory.");
         curMem = currentMemoryUsage.addAndGet(-neededChunkMemory);
-        if (getPrefetchMemRetry > MAX_NUM_OF_RETRY) {
+        if (getPrefetchMemRetry > prefetchMaxRetry) {
           logger.debug(
               "Retry limit for prefetch has been reached. Cancel reserved memory and prefetch attempt.");
           break;
