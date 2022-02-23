4fdaeabedd6dcba232e11e6811af44dbe4aa914a
Toby Zhang <toby.zhang@snowflake.com>
2022-02-23 14:23:19 -0800

    NO-SNOW: fix test failures due to invalid private key (#705)
    NO-SNOW
    
    JDBC tests start failing with invalid private key issue after 9a09d10, this PR contains the fix by setting the corresponding public key first

--- src/test/java/net/snowflake/client/AbstractDriverIT.java
+++ src/test/java/net/snowflake/client/AbstractDriverIT.java
@@ -127,9 +127,6 @@ public class AbstractDriverIT {
     String ssoPassword = TestUtil.systemGetEnv("SNOWFLAKE_TEST_SSO_PASSWORD");
     params.put("ssoPassword", ssoPassword);
 
-    String privateKeyFile = TestUtil.systemGetEnv("SNOWFLAKE_TEST_PRIVATE_KEY_FILE");
-    params.put("privateKeyFile", privateKeyFile);
-
     return params;
   }
 
