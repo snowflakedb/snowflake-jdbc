3c1c17f68a449faef27f8ea84e6f9064c6c9de70
Josh Soref <jsoref@users.noreply.github.com>
2022-05-23 23:13:54 -0400

    spelling: concurrency
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/main/java/net/snowflake/client/jdbc/SnowflakePreparedStatementV1.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakePreparedStatementV1.java
@@ -67,7 +67,7 @@ class SnowflakePreparedStatementV1 extends SnowflakeStatementV1
    * @param skipParsing true if the applications want to skip parsing to get metadata. false by
    *     default.
    * @param resultSetType result set type: ResultSet.TYPE_FORWARD_ONLY.
-   * @param resultSetConcurrency result set conconcurrency: ResultSet.CONCUR_READ_ONLY.
+   * @param resultSetConcurrency result set concurrency: ResultSet.CONCUR_READ_ONLY.
    * @param resultSetHoldability result set holdability: ResultSet.CLOSE_CURSORS_AT_COMMIT
    * @throws SQLException if any SQL error occurs.
    */
