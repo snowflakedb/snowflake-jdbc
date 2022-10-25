b4175079329ecba53a0685234c6f6e97f9cdca77
Kiran Dama <69480841+sfc-gh-kdama@users.noreply.github.com>
2022-10-25 16:18:37 -0700

    Update test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -6,7 +6,7 @@ set -o pipefail
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 JDBC_ROOT="$(cd "${THIS_DIR}/.." && pwd)"
 
-source $THIS_DIR/_init.sh
+source $THIS_DIR/_init7.sh
 source $THIS_DIR/scripts/login_internal_docker.sh
 source $THIS_DIR/scripts/set_git_info.sh
 
