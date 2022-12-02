31246f291b857e5037b2883b7db55349e53c2073
Bichitra Sahoo <bichitra.sahoo@atlan.com>
2022-12-02 13:37:00 +0530

    Handle the GEOMETRY data type by adding the switch case.

--- src/main/java/net/snowflake/client/jdbc/SnowflakeUtil.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeUtil.java
@@ -220,6 +220,11 @@ public class SnowflakeUtil {
         extColTypeName = "BINARY";
         break;
 
+      case GEOMETRY:
+        colType = 12;
+        extColTypeName = "GEOMETRY";
+        break;
+
       case GEOGRAPHY:
         colType = Types.VARCHAR;
         extColTypeName = "GEOGRAPHY";
