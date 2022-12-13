467da8a8a94c6dcc82abb6b7d90fcf22fe02528a
Ilesh garish <ilesh.garish@snowflake.com>
2022-12-13 20:54:06 +0000

    Added interface

---
+++ src/main/java/net/snowflake/client/jdbc/ResultCache/IResultCache.java
@@ -0,0 +1,30 @@
+package net.snowflake.client.jdbc.ResultCache;
+
+/** Interface for client side result cache. */
+public interface IResultCache {
+
+  /**
+   * Add result in the cache. If entry exist, overwrite it.
+   *
+   * @param queryText Query text.
+   * @param queryId Query id.
+   * @param sessionId Session id.
+   * @param result Result object of type ResultSetSerializable.
+   */
+  void putResult(String queryText, String queryId, String sessionId, Object result);
+
+  /**
+   * Get the result from the cache. If result doesn't exist then return null.
+   *
+   * @param queryText Query text.
+   * @return Result object of type ResultSetSerializable.
+   */
+  Object getResult(String queryText);
+
+  /**
+   * Clear the cache.
+   *
+   * @param queryText Query text.
+   */
+  void clean(String queryText);
+}
