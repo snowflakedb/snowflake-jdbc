80dd987a40ad6b6210c773c39327da26912fa11e
srikanth <s.kumbham@snowflake.com>
2023-04-25 18:56:10 +0000

    format issues

--- src/test/java/net/snowflake/client/jdbc/ResultSetLatestIT.java
+++ src/test/java/net/snowflake/client/jdbc/ResultSetLatestIT.java
@@ -789,25 +789,25 @@ public class ResultSetLatestIT extends ResultSet0IT {
     Statement statement = connection.createStatement();
 
     String sampleCreateTableWithAllColTypes =
-            "CREATE or replace TABLE case_sensitive ("
-                    + "  boolean_col BOOLEAN,"
-                    + "  date_col DATE,"
-                    + "  time_col TIME,"
-                    + "  timestamp_col TIMESTAMP,"
-                    + "  timestamp_ltz_col TIMESTAMP_LTZ,"
-                    + "  timestamp_ntz_col TIMESTAMP_NTZ,"
-                    + "  number_col NUMBER,"
-                    + "  float_col FLOAT,"
-                    + "  double_col DOUBLE,"
-                    + "  binary_col BINARY,"
-                    + "  geography_col GEOGRAPHY,"
-                    + "  variant_col VARIANT,"
-                    + "  object_col1 OBJECT,"
-                    + "  array_col1 ARRAY,"
-                    + "  text_col1 TEXT,"
-                    + "  varchar_col VARCHAR(16777216),"
-                    + "  char_col CHAR(16777216)"
-                    + ");";
+        "CREATE or replace TABLE case_sensitive ("
+            + "  boolean_col BOOLEAN,"
+            + "  date_col DATE,"
+            + "  time_col TIME,"
+            + "  timestamp_col TIMESTAMP,"
+            + "  timestamp_ltz_col TIMESTAMP_LTZ,"
+            + "  timestamp_ntz_col TIMESTAMP_NTZ,"
+            + "  number_col NUMBER,"
+            + "  float_col FLOAT,"
+            + "  double_col DOUBLE,"
+            + "  binary_col BINARY,"
+            + "  geography_col GEOGRAPHY,"
+            + "  variant_col VARIANT,"
+            + "  object_col1 OBJECT,"
+            + "  array_col1 ARRAY,"
+            + "  text_col1 TEXT,"
+            + "  varchar_col VARCHAR(16777216),"
+            + "  char_col CHAR(16777216)"
+            + ");";
 
     statement.execute(sampleCreateTableWithAllColTypes);
     ResultSet rs = statement.executeQuery("select * from case_sensitive");
@@ -831,8 +831,8 @@ public class ResultSetLatestIT extends ResultSet0IT {
     assertTrue(metaData.isCaseSensitive(15)); // TEXT
     assertTrue(metaData.isCaseSensitive(16)); // VARCHAR
     assertTrue(metaData.isCaseSensitive(17)); // CHAR
-
   }
+
   public void testAutoIncrementJsonResult() throws SQLException {
     Connection connection = init();
     Statement statement = connection.createStatement();
