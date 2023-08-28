9ea838e829b3e1b6ad146b708582b8e10b23edc3
Abhishek Bakare <abhishek.bakare@snowflake.com>
2023-08-28 18:17:38 +0000

    Show Pagination Driver level POC

--- src/main/java/net/snowflake/client/jdbc/SnowflakeDatabaseMetaData.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeDatabaseMetaData.java
@@ -1498,6 +1498,7 @@ public class SnowflakeDatabaseMetaData implements DatabaseMetaData {
       }
     }
 
+    showTablesCommand += " limit 1";
     logger.debug("sql command to get table metadata: {}", showTablesCommand);
 
     resultSet = executeAndReturnEmptyResultIfNotFound(statement, showTablesCommand, GET_TABLES);
@@ -1510,51 +1511,83 @@ public class SnowflakeDatabaseMetaData implements DatabaseMetaData {
         Arrays.toString(types));
 
     return new SnowflakeDatabaseMetaDataQueryResultSet(GET_TABLES, resultSet, statement) {
+
+      // maintain last table which was returned to use this information for next pagination call
+      String lastTable = null;
+
       @Override
       public boolean next() throws SQLException {
         logger.debug("public boolean next()", false);
         incrementRow();
 
-        // iterate throw the show table result until we find an entry
-        // that matches the table name
+        // get the next table
         while (showObjectResultSet.next()) {
-          String tableName = showObjectResultSet.getString(2);
-
-          String dbName;
-          String schemaName;
-          String kind;
-          String comment;
-
-          if (viewOnly) {
-            dbName = showObjectResultSet.getString(4);
-            schemaName = showObjectResultSet.getString(5);
-            kind = "VIEW";
-            comment = showObjectResultSet.getString(7);
-          } else {
-            dbName = showObjectResultSet.getString(3);
-            schemaName = showObjectResultSet.getString(4);
-            kind = showObjectResultSet.getString(5);
-            comment = showObjectResultSet.getString(6);
-          }
-
-          if ((compiledTablePattern == null || compiledTablePattern.matcher(tableName).matches())
-              && (compiledSchemaPattern == null
-                  || compiledSchemaPattern.matcher(schemaName).matches())) {
-            nextRow[0] = dbName;
-            nextRow[1] = schemaName;
-            nextRow[2] = tableName;
-            nextRow[3] = kind;
-            nextRow[4] = comment;
-            nextRow[5] = null;
-            nextRow[6] = null;
-            nextRow[7] = null;
-            nextRow[8] = null;
-            nextRow[9] = null;
-            return true;
-          }
+          if (createRow()) return true;
         }
 
-        close(); // close
+        // if no more results are found, now trigger pagination call
+        // Note: In actuality, instead of show tables command, a pagination call with a token will be invoked here.
+        // For now, a show command is invoked.
+        ResultSet resultSet1 = executeAndReturnEmptyResultIfNotFound(statement, "show /* JDBC:DatabaseMetaData.getTables() */ tables in schema \"JDBC_DB1\".\"JDBC_SCHEMA11\" limit 1 from '" + lastTable + "'", GET_TABLES);
+        sendInBandTelemetryMetadataMetrics(
+                resultSet1,
+                "getTables",
+                originalCatalog,
+                originalSchemaPattern,
+                tableNamePattern,
+                Arrays.toString(types));
+        this.showObjectResultSet = resultSet1;
+
+        // get first result from pagination call and return it as a part of next()
+        boolean foundAnyObject = false;
+        while (showObjectResultSet.next()) {
+          foundAnyObject = true;
+          if (createRow()) return true;
+        }
+
+        // if no results are found out of pagination, that means all objects are found. Close the ResultSet and show is done.
+        if (!foundAnyObject) {
+          close();
+          return false;
+        }
+        return true;
+      }
+
+      private boolean createRow() throws SQLException {
+        String tableName = showObjectResultSet.getString(2);
+
+        String dbName;
+        String schemaName;
+        String kind;
+        String comment;
+
+        if (viewOnly) {
+          dbName = showObjectResultSet.getString(4);
+          schemaName = showObjectResultSet.getString(5);
+          kind = "VIEW";
+          comment = showObjectResultSet.getString(7);
+        } else {
+          dbName = showObjectResultSet.getString(3);
+          schemaName = showObjectResultSet.getString(4);
+          kind = showObjectResultSet.getString(5);
+          comment = showObjectResultSet.getString(6);
+        }
+
+        if ((compiledTablePattern == null || compiledTablePattern.matcher(tableName).matches())
+            && (compiledSchemaPattern == null
+                || compiledSchemaPattern.matcher(schemaName).matches())) {
+          nextRow[0] = dbName;
+          nextRow[1] = schemaName;
+          nextRow[2] = lastTable = tableName;
+          nextRow[3] = kind;
+          nextRow[4] = comment;
+          nextRow[5] = null;
+          nextRow[6] = null;
+          nextRow[7] = null;
+          nextRow[8] = null;
+          nextRow[9] = null;
+          return true;
+        }
         return false;
       }
     };
