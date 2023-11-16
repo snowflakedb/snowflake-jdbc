a2717e7bf259e5292358a14928bad5cd8cb509c6
Piotr Fus <piotr.fus@snowflake.com>
2023-11-16 11:50:27 +0100

    Fix after review

--- src/main/java/net/snowflake/client/util/Predicates.java
+++
@@ -1,9 +0,0 @@
-package net.snowflake.client.util;
-
-public class Predicates {
-  public static void notNull(Object o, String msg) {
-    if (o == null) {
-      throw new NullPointerException(msg);
-    }
-  }
-}
