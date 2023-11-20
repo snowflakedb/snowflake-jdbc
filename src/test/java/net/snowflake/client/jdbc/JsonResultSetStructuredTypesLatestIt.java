06e990019cb4b4d0789bd5dbc1dcd60ea2fd1a21
Piotr Fus <piotr.fus@snowflake.com>
2023-11-20 09:32:24 +0100

    Migrate structured types tests to new file

---
+++ src/test/java/net/snowflake/client/jdbc/JsonResultSetStructuredTypesLatestIt.java
@@ -0,0 +1,7 @@
+package net.snowflake.client.jdbc;
+
+public class JsonResultSetStructuredTypesLatestIt extends BaseResultSetStructuredTypesLatestIT {
+  public JsonResultSetStructuredTypesLatestIt() {
+    super("JSON");
+  }
+}
