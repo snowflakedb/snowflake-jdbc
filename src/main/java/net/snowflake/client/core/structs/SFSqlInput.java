cbe3bbab47eea0dda8acbc26a3cfcabdc21b7c3d
Piotr Fus <piotr.fus@snowflake.com>
2023-11-20 08:58:53 +0100

    Add object in object test

--- src/main/java/net/snowflake/client/core/structs/SFSqlInput.java
+++ src/main/java/net/snowflake/client/core/structs/SFSqlInput.java
@@ -5,4 +5,5 @@ import java.sql.SQLException;
 public interface SFSqlInput {
   String readString(String fieldName) throws SQLException;
   Boolean readBoolean(String fieldName) throws SQLException;
+  <T extends SFSqlData> T readObject(String fieldName, Class<T> type) throws SQLException;
 }
