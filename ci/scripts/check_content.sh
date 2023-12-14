507f7d50c629cef0ef02d305bb9476ebee511145
Dominik Przybysz <dominik.przybysz@snowflake.com>
2023-12-14 12:13:02 +0100

    SNOW-831250: Create thin jar

--- ci/scripts/check_content.sh
+++ ci/scripts/check_content.sh
@@ -2,11 +2,13 @@
 
 # scripts used to check if all dependency is shaded into snowflake internal path
 
+package_modifier=$1
+
 set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-if jar tvf $DIR/../../target/snowflake-jdbc.jar  | awk '{print $8}' | grep -v -E "^(net|com)/snowflake" | grep -v -E "(com|net)/\$" | grep -v -E "^META-INF" | grep -v -E "^mozilla" | grep -v -E "^com/sun/jna" | grep -v com/sun/ | grep -v mime.types; then
+if jar tvf $DIR/../../target/snowflake-jdbc${package_modifier}.jar  | awk '{print $8}' | grep -v -E "^(net|com)/snowflake" | grep -v -E "(com|net)/\$" | grep -v -E "^META-INF" | grep -v -E "^mozilla" | grep -v -E "^com/sun/jna" | grep -v com/sun/ | grep -v mime.types; then
   echo "[ERROR] JDBC jar includes class not under the snowflake namespace"
   exit 1
 fi
