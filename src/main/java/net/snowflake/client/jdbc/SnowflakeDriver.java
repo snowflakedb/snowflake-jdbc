4e8f72c357ee17c33de6eb9a1afa05c39acb5847
Dominik Przybysz <132913826+sfc-gh-dprzybysz@users.noreply.github.com>
2024-01-25 13:35:05 +0100

    Bump version from 3.14.4 to 3.14.5 (#1615)

--- src/main/java/net/snowflake/client/jdbc/SnowflakeDriver.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeDriver.java
@@ -23,7 +23,7 @@ public class SnowflakeDriver implements Driver {
   static SnowflakeDriver INSTANCE;
 
   public static final Properties EMPTY_PROPERTIES = new Properties();
-  public static String implementVersion = "3.14.4";
+  public static String implementVersion = "3.14.5";
 
   static int majorVersion = 0;
   static int minorVersion = 0;
