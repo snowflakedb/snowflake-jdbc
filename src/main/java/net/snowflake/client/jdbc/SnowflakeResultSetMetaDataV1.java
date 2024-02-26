1de537d9c8b5a31746f75e2e57c572472d4d59cf
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2024-02-26 17:35:27 +0100

    SNOW-970859 Fix of snowflake types mapping.

--- src/main/java/net/snowflake/client/jdbc/SnowflakeResultSetMetaDataV1.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeResultSetMetaDataV1.java
@@ -106,7 +106,7 @@ class SnowflakeResultSetMetaDataV1 implements ResultSetMetaData, SnowflakeResult
     int colType = getColumnType(column);
 
     switch (colType) {
-        // Note: SF types ARRAY, OBJECT, GEOMETRY are also represented as VARCHAR.
+        // Note: SF types ARRAY, GEOGRAPHY, GEOMETRY are also represented as VARCHAR.
       case Types.VARCHAR:
       case Types.CHAR:
       case Types.STRUCT:
