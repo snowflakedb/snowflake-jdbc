5bb09bff36222bb66ce08c9b105faa071c3d9af4
Piotr Fus <piotr.fus@snowflake.com>
2024-03-12 12:08:02 +0100

    SNOW-1203558 Fix geo types values (#1663)

--- src/main/java/net/snowflake/client/jdbc/SnowflakeUtil.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeUtil.java
@@ -277,8 +277,12 @@ public class SnowflakeUtil {
         break;
 
       case OBJECT:
+        int targetType =
+            "GEOGRAPHY".equals(extColTypeName) || "GEOMETRY".equals(extColTypeName)
+                ? Types.VARCHAR
+                : Types.STRUCT;
         columnTypeInfo =
-            new ColumnTypeInfo(Types.STRUCT, defaultIfNull(extColTypeName, "OBJECT"), baseType);
+            new ColumnTypeInfo(targetType, defaultIfNull(extColTypeName, "OBJECT"), baseType);
         break;
 
       case VARIANT:
