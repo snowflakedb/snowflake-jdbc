2a2c6f8c74298c55eaedbdaa8af674f5ce28bdda
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2024-04-12 13:02:48 +0200

    Structured type perf test

--- src/test/java/net/snowflake/client/jdbc/structuredtypes/ResultSetStructuredPerfLatestIT.java
+++ src/test/java/net/snowflake/client/jdbc/structuredtypes/ResultSetStructuredPerfLatestIT.java
@@ -9,6 +9,7 @@ import net.snowflake.client.ThrowingConsumer;
 import net.snowflake.client.category.TestCategoryResultSet;
 import net.snowflake.client.core.structs.SnowflakeObjectTypeFactories;
 import net.snowflake.client.jdbc.BaseJDBCTest;
+import net.snowflake.client.jdbc.SnowflakePreparedStatementV1;
 import net.snowflake.client.jdbc.structuredtypes.sqldata.AllTypesClass;
 import net.snowflake.client.jdbc.structuredtypes.sqldata.SimpleClass;
 import org.junit.Before;
@@ -91,6 +92,34 @@ public class ResultSetStructuredPerfLatestIT extends BaseJDBCTest {
             for (int i = 0; i < count; i++) {
                 resultSet.next();
                 result.add(resultSet.getObject(1, SimpleClass.class));
+            }
+            System.out.println("Finish" + LocalDateTime.now());
+        }
+    }
+
+    @Test
+    @ConditionalIgnoreRule.ConditionalIgnore(condition = RunningOnGithubAction.class)
+    public void testWriteReadManyStrings() throws SQLException {
+        SnowflakeObjectTypeFactories.register(SimpleClass.class, SimpleClass::new);
+        int count = 100000;
+        try (Connection connection = init()) {
+            Statement statement = connection.createStatement();
+            statement.execute(
+                    "CREATE OR REPLACE TABLE test_table (ob VARCHAR)");
+
+
+            statement.execute("insert into test_table select randstr(1000,random()) from table(generator(rowcount=>"+count+"))");
+            System.out.println("Start" + LocalDateTime.now());
+
+            PreparedStatement stmt3 =
+                            connection.prepareStatement("SELECT ob FROM test_table");
+
+            ResultSet resultSet = stmt3.executeQuery();
+            System.out.println("Start2" + LocalDateTime.now());
+            List<String> result = new ArrayList<>();
+            for (int i = 0; i < count; i++) {
+                resultSet.next();
+                result.add(resultSet.getString(1));
             }
             System.out.println("Finish" + LocalDateTime.now());
         }
