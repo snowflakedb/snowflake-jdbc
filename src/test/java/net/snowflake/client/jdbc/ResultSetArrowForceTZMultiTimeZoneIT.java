7a368bbaf26facfa4385033f699d73999f539cc5
sfc-gh-ext-simba-jy <jonghun.yun@insightsoftware.com>
2024-04-19 00:20:48 -0700

    lint fix

--- src/test/java/net/snowflake/client/jdbc/ResultSetArrowForceTZMultiTimeZoneIT.java
+++ src/test/java/net/snowflake/client/jdbc/ResultSetArrowForceTZMultiTimeZoneIT.java
@@ -7,9 +7,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import java.sql.Connection;
-import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import net.snowflake.client.category.TestCategoryArrow;
 import org.apache.commons.lang3.StringUtils;
@@ -40,25 +40,25 @@ public class ResultSetArrowForceTZMultiTimeZoneIT extends ResultSetArrowForce0Mu
 
   private void testTimestampTZWithScale(int scale) throws SQLException {
     String[] cases = {
-            "2017-01-01 12:00:00 Z",
-            "2014-01-02 16:00:00 Z",
-            "2014-01-02 12:34:56 Z",
-            "1970-01-01 00:00:00 Z",
-            "1970-01-01 00:00:01 Z",
-            "1969-12-31 11:59:59 Z",
-            "0000-01-01 00:00:01 Z",
-            "0001-12-31 11:59:59 Z"
+      "2017-01-01 12:00:00 Z",
+      "2014-01-02 16:00:00 Z",
+      "2014-01-02 12:34:56 Z",
+      "1970-01-01 00:00:00 Z",
+      "1970-01-01 00:00:01 Z",
+      "1969-12-31 11:59:59 Z",
+      "0000-01-01 00:00:01 Z",
+      "0001-12-31 11:59:59 Z"
     };
 
     long[] times = {
-            1483272000000L,
-            1388678400000L,
-            1388666096000L,
-            0,
-            1000,
-            -43201000L,
-            -62167391999000L,
-            -62104276801000L
+      1483272000000L,
+      1388678400000L,
+      1388666096000L,
+      0,
+      1000,
+      -43201000L,
+      -62167391999000L,
+      -62104276801000L
     };
 
     String table = "test_arrow_ts_tz";
@@ -67,8 +67,8 @@ public class ResultSetArrowForceTZMultiTimeZoneIT extends ResultSetArrowForce0Mu
 
     String values = "('" + StringUtils.join(cases, "'),('") + "'), (null)";
     try (Connection con = init(table, column, values);
-         Statement statement = con.createStatement();
-         ResultSet rs = statement.executeQuery("select * from " + table)) {
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery("select * from " + table)) {
       try {
         int i = 0;
         while (i < cases.length) {
@@ -88,27 +88,27 @@ public class ResultSetArrowForceTZMultiTimeZoneIT extends ResultSetArrowForce0Mu
   @Test
   public void testTimestampTZWithNanos() throws SQLException {
     String[] cases = {
-            "2017-01-01 12:00:00.1",
-            "2014-01-02 16:00:00.123456789",
-            "2014-01-02 12:34:56.999999999",
-            "1969-12-31 23:59:59.000000001",
-            "1970-01-01 00:00:00.000000001",
-            "1970-01-01 00:00:01.0000001",
-            "1969-12-31 11:59:59.134",
-            "0001-12-31 11:59:59.234141",
-            "0000-01-01 00:00:01.790870987"
+      "2017-01-01 12:00:00.1",
+      "2014-01-02 16:00:00.123456789",
+      "2014-01-02 12:34:56.999999999",
+      "1969-12-31 23:59:59.000000001",
+      "1970-01-01 00:00:00.000000001",
+      "1970-01-01 00:00:01.0000001",
+      "1969-12-31 11:59:59.134",
+      "0001-12-31 11:59:59.234141",
+      "0000-01-01 00:00:01.790870987"
     };
 
     long[] times = {
-            1483272000100L,
-            1388678400123L,
-            1388666096999L,
-            -1000,
-            0,
-            1000,
-            -43200866,
-            -62104276800766L,
-            -62167391998210L
+      1483272000100L,
+      1388678400123L,
+      1388666096999L,
+      -1000,
+      0,
+      1000,
+      -43200866,
+      -62104276800766L,
+      -62167391998210L
     };
 
     int[] nanos = {100000000, 123456789, 999999999, 1, 1, 100, 134000000, 234141000, 790870987};
@@ -119,8 +119,8 @@ public class ResultSetArrowForceTZMultiTimeZoneIT extends ResultSetArrowForce0Mu
 
     String values = "('" + StringUtils.join(cases, " Z'),('") + " Z'), (null)";
     try (Connection con = init(table, column, values);
-         Statement statement = con.createStatement();
-         ResultSet rs = statement.executeQuery("select * from " + table)) {
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery("select * from " + table)) {
       try {
         int i = 0;
         while (i < cases.length) {
@@ -145,31 +145,31 @@ public class ResultSetArrowForceTZMultiTimeZoneIT extends ResultSetArrowForce0Mu
   @Test
   public void testTimestampTZWithMicros() throws SQLException {
     String[] cases = {
-            "2017-01-01 12:00:00.1",
-            "2014-01-02 16:00:00.123456",
-            "2014-01-02 12:34:56.999999",
-            "1969-12-31 23:59:59.000001",
-            "1970-01-01 00:00:00.000001",
-            "1970-01-01 00:00:01.00001",
-            "1969-12-31 11:59:59.134",
-            "0001-12-31 11:59:59.234141",
-            "0000-01-01 00:00:01.79087"
+      "2017-01-01 12:00:00.1",
+      "2014-01-02 16:00:00.123456",
+      "2014-01-02 12:34:56.999999",
+      "1969-12-31 23:59:59.000001",
+      "1970-01-01 00:00:00.000001",
+      "1970-01-01 00:00:01.00001",
+      "1969-12-31 11:59:59.134",
+      "0001-12-31 11:59:59.234141",
+      "0000-01-01 00:00:01.79087"
     };
 
     long[] times = {
-            1483272000100L,
-            1388678400123L,
-            1388666096999L,
-            -1000,
-            0,
-            1000,
-            -43200866,
-            -62104276800766L,
-            -62167391998210L
+      1483272000100L,
+      1388678400123L,
+      1388666096999L,
+      -1000,
+      0,
+      1000,
+      -43200866,
+      -62104276800766L,
+      -62167391998210L
     };
 
     int[] nanos = {
-            100000000, 123456000, 999999000, 1000, 1000, 10000, 134000000, 234141000, 790870000
+      100000000, 123456000, 999999000, 1000, 1000, 10000, 134000000, 234141000, 790870000
     };
 
     String table = "test_arrow_ts_tz";
@@ -178,8 +178,8 @@ public class ResultSetArrowForceTZMultiTimeZoneIT extends ResultSetArrowForce0Mu
 
     String values = "('" + StringUtils.join(cases, " Z'),('") + " Z'), (null)";
     try (Connection con = init(table, column, values);
-         Statement statement = con.createStatement();
-         ResultSet rs = statement.executeQuery("select * from " + table)) {
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery("select * from " + table)) {
       try {
         int i = 0;
         while (i < cases.length) {
