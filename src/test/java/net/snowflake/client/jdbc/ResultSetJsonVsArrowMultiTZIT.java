bc4114472f09bec451cd031a92f612772b224007
sfc-gh-ext-simba-jy <jonghun.yun@insightsoftware.com>
2024-04-22 18:32:58 -0700

    fix

--- src/test/java/net/snowflake/client/jdbc/ResultSetJsonVsArrowMultiTZIT.java
+++ src/test/java/net/snowflake/client/jdbc/ResultSetJsonVsArrowMultiTZIT.java
@@ -3,8 +3,9 @@
  */
 package net.snowflake.client.jdbc;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
