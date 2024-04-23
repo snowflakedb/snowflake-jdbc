64607e2bd1c151188199fa8175be3e22a6c2eb61
norrislee <norris.lee@insightsoftware.com>
2024-04-23 09:11:42 -0700

    revert change for datetimeconverter

--- src/main/java/net/snowflake/client/core/SFJsonResultSet.java
+++ src/main/java/net/snowflake/client/core/SFJsonResultSet.java
@@ -253,9 +253,7 @@ public abstract class SFJsonResultSet extends SFBaseResultSet {
     int columnType = resultSetMetaData.getColumnType(columnIndex);
     int columnSubType = resultSetMetaData.getInternalColumnType(columnIndex);
     int scale = resultSetMetaData.getScale(columnIndex);
-    return converters
-        .getDateTimeConverter()
-        .getDate(obj, columnType, columnSubType, TimeZone.getDefault(), scale);
+    return converters.getDateTimeConverter().getDate(obj, columnType, columnSubType, tz, scale);
   }
 
   @Override
