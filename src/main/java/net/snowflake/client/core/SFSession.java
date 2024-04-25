0ba34aff9975e271848434f1cff488f21db44a54
Jelena Furundzic <jelena.furundzic@insightsoftware.com>
2024-04-25 04:30:18 -0700

    Add queryid to all exceptions

--- src/main/java/net/snowflake/client/core/SFSession.java
+++ src/main/java/net/snowflake/client/core/SFSession.java
@@ -223,7 +223,10 @@ public class SFSession extends SFBaseSession {
         jsonNode = OBJECT_MAPPER.readTree(response);
       } catch (Exception e) {
         throw new SnowflakeSQLLoggedException(
-            this, e.getMessage(), "No response or invalid response from GET request. Error: {}");
+            queryID,
+            this,
+            e.getMessage(),
+            "No response or invalid response from GET request. Error: {}");
       }
 
       // Get response as JSON and parse it to get the query status
@@ -257,7 +260,7 @@ public class SFSession extends SFBaseSession {
             else if (ex instanceof SFException) {
               throw new SnowflakeSQLException((SFException) ex);
             }
-            throw new SnowflakeSQLException(ex.getMessage());
+            throw new SnowflakeSQLException(queryID, ex.getMessage());
           }
           sessionRenewed = true;
           // If the error code was not due to session renewal issues, throw an exception
