c90393334b56078e61f97139e78e979782201285
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2024-04-27 19:32:14 +0200

    SNOW-1232333 - ResultSet getObject method return string if type wasn't specified

--- src/main/java/net/snowflake/client/core/SFSqlInput.java
+++ src/main/java/net/snowflake/client/core/SFSqlInput.java
@@ -4,7 +4,6 @@
 package net.snowflake.client.core;
 
 import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
 import java.sql.SQLInput;
 import java.util.List;
 import java.util.Map;
