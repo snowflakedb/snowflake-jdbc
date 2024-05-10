37f2348e85a21bd7da0886ad80d191fd5dba50a2
sfc-gh-ext-simba-jy <jonghun.yun@insightsoftware.com>
2024-05-10 14:53:13 -0700

    change

--- src/test/java/net/snowflake/client/core/SessionUtilLatestIT.java
+++ src/test/java/net/snowflake/client/core/SessionUtilLatestIT.java
@@ -403,7 +403,7 @@ public class SessionUtilLatestIT extends BaseJDBCTest {
     input.setLoginTimeout(1000);
     input.setSessionParameters(new HashMap<>());
     input.setAuthenticator("https://testauth.okta.com");
-    input.setDisableSamlUrlCheck(false);
+    input.setDisableSamlUrlCheck(true);
     return input;
   }
 
