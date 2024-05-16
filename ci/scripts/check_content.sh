71f8615c72307efa988e16dc3e63541af43d9282
sfc-gh-ext-simba-jy <jonghun.yun@insightsoftware.com>
2024-05-15 19:04:30 -0700

    checking

--- ci/scripts/check_content.sh
+++ ci/scripts/check_content.sh
@@ -8,6 +8,8 @@ set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+echo $DIR/../../target/snowflake-jdbc${package_modifier}.jar
+
 if jar tvf $DIR/../../target/snowflake-jdbc${package_modifier}.jar  | awk '{print $8}' | grep -v -E "^(net|com)/snowflake" | grep -v -E "(com|net)/\$" | grep -v -E "^META-INF" | grep -v -E "^mozilla" | grep -v -E "^com/sun/jna" | grep -v com/sun/ | grep -v mime.types; then
   echo "[ERROR] JDBC jar includes class not under the snowflake namespace"
   exit 1
