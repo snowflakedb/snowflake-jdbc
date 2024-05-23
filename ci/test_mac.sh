9b108ddc8a80b7124f5b4bedcaea5bc8cb39de4b
Juan Martinez Ramirez <juan.martinezramirez@snowflake.com>
2024-05-23 11:38:24 -0600

    Applying PR suggestions

--- ci/test_mac.sh
+++ ci/test_mac.sh
@@ -12,11 +12,10 @@ source $THIS_DIR/scripts/set_git_info.sh
 
 export WORKSPACE=$GITHUB_WORKSPACE
 export SOURCE_ROOT=$GITHUB_WORKSPACE
-export SHOULD_GENERATE_COVERAGE_REPORT=1
 
 python3 --version
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -U pip
 pip3 install -U snowflake-connector-python
-$THIS_DIR/container/test_component.sh $SHOULD_GENERATE_COVERAGE_REPORT
+$THIS_DIR/container/test_component.sh
