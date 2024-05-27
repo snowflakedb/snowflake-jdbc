69619036fb7506e330a90c117dfe765999906fd8
Dominik Przybysz <dominik.przybysz@snowflake.com>
2024-05-27 09:48:18 +0200

    SNOW-1437655: Set auth timeout to 0 in not auth endpoint calls

--- src/main/java/net/snowflake/client/core/SFSession.java
+++ src/main/java/net/snowflake/client/core/SFSession.java
@@ -229,7 +229,7 @@ public class SFSession extends SFBaseSession {
             HttpUtil.executeGeneralRequest(
                 get,
                 loginTimeout,
-                authTimeout,
+                0,
                 (int) httpClientSocketTimeout.toMillis(),
                 maxHttpRetries,
                 getHttpClientKey());
@@ -1136,7 +1136,7 @@ public class SFSession extends SFBaseSession {
             HttpUtil.executeGeneralRequest(
                 postRequest,
                 SF_HEARTBEAT_TIMEOUT,
-                authTimeout,
+                0,
                 (int) httpClientSocketTimeout.toMillis(),
                 0,
                 getHttpClientKey());
