75c45f6345b83dad876f7c0acfd3a05713fbcde0
Juan Martinez Ramirez <juan.martinezramirez@snowflake.com>
2024-05-30 14:30:29 -0600

    Add ignore condition to specify which test should be ignored depending on the cloud provider

---
+++ src/test/java/net/snowflake/client/RunningNotOnAWS.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2012-2019 Snowflake Computing Inc. All right reserved.
+ */
+package net.snowflake.client;
+
+/** Run tests only on specified cloud provider or ignore */
+public class RunningNotOnAWS implements ConditionalIgnoreRule.IgnoreCondition {
+  public boolean isSatisfied() {
+    String cloudProvider = TestUtil.systemGetEnv("JDBC_TEST_CLOUD_PROVIDER");
+    return cloudProvider != null && !cloudProvider.equalsIgnoreCase("AWS");
+  }
+}
