0b0c077b674a62ede06d6605f5245309e84ba65a
Juan Martinez Ramirez <juan.martinezramirez@snowflake.com>
2024-06-06 14:36:05 -0600

    Deprecated methods from TestUtil.systemGetEnv required only for compatibility with old driver.

--- src/test/java/net/snowflake/client/TestUtil.java
+++ src/test/java/net/snowflake/client/TestUtil.java
@@ -66,13 +66,14 @@ public class TestUtil {
    * System.getenv wrapper. If System.getenv raises an SecurityException, it is ignored and returns
    * null.
    *
-   * <p>This is replicated from SnowflakeUtil.systemGetEnv, because the old driver doesn't have that
-   * function for the tests to use it. Replace this function call with SnowflakeUtil.systemGetEnv
-   * when it is available.
-   *
+   * @deprecated This method should be replaced by SnowflakeUtil.systemGetEnv.
+   *     <p>This is replicated from SnowflakeUtil.systemGetEnv, because the old driver doesn't have
+   *     that function for the tests to use it. Replace this function call with
+   *     SnowflakeUtil.systemGetEnv when it is available.
    * @param env the environment variable name.
    * @return the environment variable value if set, otherwise null.
    */
+  @Deprecated
   public static String systemGetEnv(String env) {
     try {
       String value = System.getenv(env);
