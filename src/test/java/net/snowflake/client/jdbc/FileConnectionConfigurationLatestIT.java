0febe450da8a100d71cdb1ac6dd056ff64d985f7
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2024-06-18 13:02:30 +0200

    SNOW-1454054 - Read connection configuration from file.

--- src/test/java/net/snowflake/client/jdbc/FileConnectionConfigurationLatestIT.java
+++ src/test/java/net/snowflake/client/jdbc/FileConnectionConfigurationLatestIT.java
@@ -10,10 +10,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /** This test could be run only on environment where file connection.toml is configured */
-// @Ignore
+ @Ignore
 public class FileConnectionConfigurationLatestIT {
 
   public static final String SNOWFLAKE_DEFAULT_CONNECTION_NAME_ENV_VAR =
