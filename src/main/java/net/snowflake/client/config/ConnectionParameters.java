ccee1b1dead6d2cb39a270b1cd5bfea874c2b8cc
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2024-06-24 16:33:46 +0200

    SNOW-1454054 - Read connection configuration from file. (#1780)
    * SNOW-1454054 - Read connection configuration from file.

---
+++ src/main/java/net/snowflake/client/config/ConnectionParameters.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
+ */
+package net.snowflake.client.config;
+
+import java.util.Properties;
+import net.snowflake.client.core.SnowflakeJdbcInternalApi;
+
+@SnowflakeJdbcInternalApi
+public class ConnectionParameters {
+  private final String url;
+  private final Properties params;
+
+  public ConnectionParameters(String uri, Properties params) {
+    this.url = uri;
+    this.params = params;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public Properties getParams() {
+    return params;
+  }
+}
