f8d9575df4b04f0ed9571533c040e7d2bf9f7ed7
Adam Kolodziejczyk <adam.kolodziejczyk@snowflake.com>
2024-06-28 14:56:43 +0200

    add proxy tests using wiremock

---
+++ src/test/java/net/snowflake/client/RunningNotOnJava8Java21.java
@@ -0,0 +1,15 @@
+package net.snowflake.client;
+
+import static net.snowflake.client.jdbc.SnowflakeUtil.systemGetProperty;
+
+public class RunningNotOnJava8Java21 implements ConditionalIgnoreRule.IgnoreCondition {
+  public boolean isSatisfied() {
+    return systemGetProperty("java.version").startsWith("1.8.0")
+        || systemGetProperty("java.version").startsWith("21.");
+  }
+
+  public static boolean isRunningOnJava8Java21() {
+    return systemGetProperty("java.version").startsWith("1.8.0")
+        || systemGetProperty("java.version").startsWith("21.");
+  }
+}
