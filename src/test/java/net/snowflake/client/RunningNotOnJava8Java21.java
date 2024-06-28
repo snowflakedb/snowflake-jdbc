728f278de033ef76794a870f76a1064c7e3e2962
Adam Kolodziejczyk <adam.kolodziejczyk@snowflake.com>
2024-06-28 17:22:05 +0200

    code review changes

--- src/test/java/net/snowflake/client/RunningNotOnJava8Java21.java
+++ src/test/java/net/snowflake/client/RunningNotOnJava8Java21.java
@@ -4,8 +4,7 @@ import static net.snowflake.client.jdbc.SnowflakeUtil.systemGetProperty;
 
 public class RunningNotOnJava8Java21 implements ConditionalIgnoreRule.IgnoreCondition {
   public boolean isSatisfied() {
-    return systemGetProperty("java.version").startsWith("1.8.0")
-        || systemGetProperty("java.version").startsWith("21.");
+    return isRunningOnJava8Java21();
   }
 
   public static boolean isRunningOnJava8Java21() {
