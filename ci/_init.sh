65b371a90bc67910db03c3bce5e8703c7b4a67bb
Jelena Furundzic <jelena.furundzic@insightsoftware.com>
2024-07-11 01:25:29 -0700

    Revert "SNOW-1490931: Preparation for Java 21 (#1796)"
    This reverts commit a4db3096c3282eb0c8aa7b86229d063a4f9ae694.

--- ci/_init.sh
+++ ci/_init.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/env bash
+#!/usr/bin/env bash
 set -e
 
 export PLATFORM=$(echo $(uname) | tr '[:upper:]' '[:lower:]')
@@ -23,7 +23,6 @@ declare -A TEST_IMAGE_NAMES=(
     [$DRIVER_NAME-centos7-openjdk8]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-centos7-openjdk8-test:$TEST_IMAGE_VERSION
     [$DRIVER_NAME-centos7-openjdk11]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-centos7-openjdk11-test:$TEST_IMAGE_VERSION
     [$DRIVER_NAME-centos7-openjdk17]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-centos7-openjdk17-test:$TEST_IMAGE_VERSION
-    [$DRIVER_NAME-centos7-openjdk21]=$DOCKER_REGISTRY_NAME/client-$DRIVER_NAME-centos7-openjdk21-test:$TEST_IMAGE_VERSION
 )
 export TEST_IMAGE_NAMES
 
@@ -31,13 +30,11 @@ declare -A TEST_IMAGE_DOCKERFILES=(
     [$DRIVER_NAME-centos7-openjdk8]=jdbc-centos7-openjdk-test
     [$DRIVER_NAME-centos7-openjdk11]=jdbc-centos7-openjdk-test
     [$DRIVER_NAME-centos7-openjdk17]=jdbc-centos7-openjdk-test
-    [$DRIVER_NAME-centos7-openjdk21]=jdbc-centos7-openjdk-test
 )
 
 declare -A TEST_IMAGE_BUILD_ARGS=(
     [$DRIVER_NAME-centos7-openjdk8]="--target jdbc-centos7-openjdk-yum --build-arg=JDK_PACKAGE=java-1.8.0-openjdk-devel"
     [$DRIVER_NAME-centos7-openjdk11]="--target jdbc-centos7-openjdk-yum --build-arg=JDK_PACKAGE=java-11-openjdk-devel" # pragma: allowlist secret
     [$DRIVER_NAME-centos7-openjdk17]="--target jdbc-centos7-openjdk17"
-    [$DRIVER_NAME-centos7-openjdk21]="--target jdbc-centos7-openjdk21"
 )
 
