0ea6360d678eff958228b4fdf58cb42f6317e9de
Dawid Heyman <dawid.heyman@snowflake.com>
2024-07-23 10:26:51 +0200

    Checkstyle

--- src/main/java/net/snowflake/client/jdbc/SnowflakeRichResultSetSerializableV1.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeRichResultSetSerializableV1.java
@@ -36,8 +36,8 @@ public class SnowflakeRichResultSetSerializableV1 extends SnowflakeResultSetSeri
   transient JsonNode richResultFirstChunkRowset = null;
 
   /**
-   * A factory function for internal usage only
-   * It creates SnowflakeRichResultSetSerializableV1 with NoOpChunksDownloader which disables chunks prefetch
+   * A factory function for internal usage only It creates SnowflakeRichResultSetSerializableV1 with
+   * NoOpChunksDownloader which disables chunks prefetch
    */
   public static SnowflakeRichResultSetSerializableV1 createWithChunksPrefetchDisabled(
       JsonNode rootNode,
