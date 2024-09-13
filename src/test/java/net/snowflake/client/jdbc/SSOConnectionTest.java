f8e5c55cc39d289d4f9f2b71ca975d3e078e269f
Eugene Clark <ehclark@partners.org>
2024-09-13 16:42:06 -0400

    Fallback to terminal based SSO authentication similar to Python connector

--- src/test/java/net/snowflake/client/jdbc/SSOConnectionTest.java
+++ src/test/java/net/snowflake/client/jdbc/SSOConnectionTest.java
@@ -63,6 +63,12 @@ class MockAuthExternalBrowserHandlers
   public void output(String msg) {
     // nop. No output
   }
+  
+  @Override
+  public String input() {
+    // nop. No input
+    return null;
+  }
 }
 
 class FakeSessionUtilExternalBrowser extends SessionUtilExternalBrowser {
