e6194556d490536bda6d2e8abdd70c2955aeb503
Dominik Przybysz <dominik.przybysz@snowflake.com>
2024-11-04 13:21:30 +0100

    SNOW-1782852: Testing arrow too many rows

--- src/test/java/net/snowflake/client/jdbc/ResultSetArrowLatestIT.java
+++ src/test/java/net/snowflake/client/jdbc/ResultSetArrowLatestIT.java
@@ -3,7 +3,16 @@
  */
 package net.snowflake.client.jdbc;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 import net.snowflake.client.category.TestCategoryArrow;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 /**
@@ -14,5 +23,32 @@ import org.junit.experimental.categories.Category;
 public class ResultSetArrowLatestIT extends ResultSetLatestIT {
   public ResultSetArrowLatestIT() {
     super("arrow");
+  }
+
+  // TODO move to parent - for now we only want to test ARROW
+  @Test
+  public void testChunkedResultSetHasExpectedNumberOfRows() throws SQLException {
+    int rowsToGet = 8295305;
+    int count = 0;
+    int seq8 = -1;
+    Map<Integer, Integer> seq8Usages = new HashMap<>();
+    try (Statement statement = connection.createStatement();
+        ResultSet resultSet =
+            statement.executeQuery(
+                "select randstr(abs(mod(random(), 20)) + 1 ,random()), seq8() as s from table(generator(rowcount=>"
+                    + rowsToGet
+                    + ")) where 1=1 and s > -1 "
+                    + "order by s")) {
+      while (resultSet.next()) {
+        int newSeq8 = resultSet.getInt(2);
+        //        System.out.println(newSeq8 + ": " + resultSet.getString(1));
+        count++;
+        assertTrue(seq8 < newSeq8);
+        seq8 = newSeq8;
+        seq8Usages.put(newSeq8, seq8Usages.getOrDefault(newSeq8, 0));
+      }
+      assertEquals(rowsToGet, count);
+      assertEquals(rowsToGet, seq8Usages.size());
+    }
   }
 }
