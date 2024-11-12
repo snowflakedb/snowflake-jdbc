5729e2251c1b7a7cb5a10372f78bf3c620941203
sfc-gh-astachowski <antoni.stachowski@snowflake.com>
2024-11-12 09:41:43 +0100

    Removed old ignore conditions and test categories

--- src/test/java/net/snowflake/client/RunningOnTestaccount.java
+++
@@ -1,8 +0,0 @@
-package net.snowflake.client;
-
-@Deprecated
-public class RunningOnTestaccount {
-  public boolean isSatisfied() {
-    return TestUtil.systemGetEnv("SNOWFLAKE_TEST_ACCOUNT").contains("testaccount");
-  }
-}
