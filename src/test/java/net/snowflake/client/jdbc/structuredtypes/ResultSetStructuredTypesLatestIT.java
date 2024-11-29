aae2c84e4072444ebf97471e3c60e6d395fc2d5d
Mikołaj Kubik <mikolaj.kubik@snowflake.com>
2024-11-29 15:15:33 +0100

    fix formatting

--- src/test/java/net/snowflake/client/jdbc/structuredtypes/ResultSetStructuredTypesLatestIT.java
+++ src/test/java/net/snowflake/client/jdbc/structuredtypes/ResultSetStructuredTypesLatestIT.java
@@ -6,7 +6,6 @@ package net.snowflake.client.jdbc.structuredtypes;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
