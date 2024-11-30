5c50919adb0b350d59201fca5a564ead06f2e4a1
Dawid Heyman <dawid.heyman@snowflake.com>
2024-11-30 22:44:59 +0100

    Refactor

--- src/main/java/net/snowflake/client/core/auth/oauth/OauthAccessTokenProvider.java
+++ src/main/java/net/snowflake/client/core/auth/oauth/OauthAccessTokenProvider.java
@@ -2,7 +2,9 @@ package net.snowflake.client.core.auth.oauth;
 
 import net.snowflake.client.core.SFException;
 import net.snowflake.client.core.SFLoginInput;
+import net.snowflake.client.core.SnowflakeJdbcInternalApi;
 
+@SnowflakeJdbcInternalApi
 public interface OauthAccessTokenProvider {
 
     String getAccessToken(SFLoginInput loginInput) throws SFException;
