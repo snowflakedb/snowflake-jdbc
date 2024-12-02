eba48e161b957faa195ef50169e61e9cc71e39f2
bpackter <bpackter@paloaltonetworks.com>
2024-12-02 11:01:35 +0200

    use file

--- src/main/java/net/snowflake/client/jdbc/RestRequest.java
+++ src/main/java/net/snowflake/client/jdbc/RestRequest.java
@@ -6,6 +6,7 @@ package net.snowflake.client.jdbc;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 import javax.net.ssl.SSLHandshakeException;
@@ -269,6 +270,7 @@ public class RestRequest {
 
         httpRequest.setURI(builder.build());
         execTimeData.setHttpClientStart();
+        logger.info("headers: " + Arrays.toString(httpRequest.getAllHeaders()));
         response = httpClient.execute(httpRequest);
         execTimeData.setHttpClientEnd();
       } catch (IllegalStateException ex) {
