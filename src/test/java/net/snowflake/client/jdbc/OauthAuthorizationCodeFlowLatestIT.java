3c168818c7a726834ea48485326cb5b9d454ea2b
Dawid Heyman <dawid.heyman@snowflake.com>
2024-12-04 23:46:55 +0100

    Extracted wiremock config to files

--- src/test/java/net/snowflake/client/jdbc/OauthAuthorizationCodeFlowLatestIT.java
+++ src/test/java/net/snowflake/client/jdbc/OauthAuthorizationCodeFlowLatestIT.java
@@ -27,174 +27,25 @@ import org.slf4j.LoggerFactory;
 @Tag(TestTags.CORE)
 public class OauthAuthorizationCodeFlowLatestIT extends BaseWiremockTest {
 
+  private static final String SCENARIOS_BASE_DIR = "/oauth/authorization_code";
   private static final String SUCCESSFUL_FLOW_SCENARIO_MAPPINGS =
-      "{\n"
-          + "    \"mappings\": [\n"
-          + "        {\n"
-          + "            \"scenarioName\": \"Successful OAuth authorization code flow\",\n"
-          + "            \"requiredScenarioState\": \"Started\",\n"
-          + "            \"newScenarioState\": \"Authorized\",\n"
-          + "            \"request\": {\n"
-          + "                \"urlPathPattern\": \"/oauth/authorize.*\",\n"
-          + "                \"method\": \"GET\"\n"
-          + "            },\n"
-          + "            \"response\": {\n"
-          + "                \"status\": 200\n"
-          + "            },\n"
-          + "            \"serveEventListeners\": [\n"
-          + "                {\n"
-          + "                    \"name\": \"webhook\",\n"
-          + "                    \"parameters\": {\n"
-          + "                        \"method\": \"GET\",\n"
-          + "                        \"url\": \"http://localhost:8001/snowflake/oauth-redirect?code=123\"\n"
-          + "                    }\n"
-          + "                }\n"
-          + "            ]\n"
-          + "        },\n"
-          + "        {\n"
-          + "            \"scenarioName\": \"Successful OAuth authorization code flow\",\n"
-          + "            \"requiredScenarioState\": \"Authorized\",\n"
-          + "            \"newScenarioState\": \"Acquired access token\",\n"
-          + "            \"request\": {\n"
-          + "                \"urlPathPattern\": \"/oauth/token-request.*\",\n"
-          + "                \"method\": \"POST\",\n"
-          + "                \"headers\": {\n"
-          + "                    \"Authorization\": {\n"
-          + "                        \"contains\": \"Basic\"\n"
-          + "                    },\n"
-          + "                    \"Content-Type\": {\n"
-          + "                        \"contains\": \"application/x-www-form-urlencoded; charset=UTF-8\"\n"
-          + "                    }\n"
-          + "                },\n"
-          + "                \"bodyPatterns\": [{\n"
-          + "                    \"contains\": \"grant_type=authorization_code&code=123&redirect_uri=http%3A%2F%2Flocalhost%3A8001%2Fsnowflake%2Foauth-redirect&code_verifier=\"\n"
-          + "                }]\n"
-          + "            },\n"
-          + "            \"response\": {\n"
-          + "                \"status\": 200,\n"
-          + "                \"body\": \"{ \\\"access_token\\\" : \\\"access-token-123\\\", \\\"refresh_token\\\" : \\\"123\\\", \\\"token_type\\\" : \\\"Bearer\\\", \\\"username\\\" : \\\"user\\\", \\\"scope\\\" : \\\"refresh_token session:role:ANALYST\\\", \\\"expires_in\\\" : 600, \\\"refresh_token_expires_in\\\" : 86399, \\\"idpInitiated\\\" : false }\"\n"
-          + "            }\n"
-          + "        }\n"
-          + "    ],\n"
-          + "    \"importOptions\": {\n"
-          + "        \"duplicatePolicy\": \"IGNORE\",\n"
-          + "        \"deleteAllNotInImport\": true\n"
-          + "    }\n"
-          + "}";
-
+      SCENARIOS_BASE_DIR + "/successful_scenario_mapping.json";
   private static final String BROWSER_TIMEOUT_SCENARIO_MAPPING =
-      "{\n"
-          + "    \"mappings\": [\n"
-          + "        {\n"
-          + "            \"scenarioName\": \"Browser Authorization timeout\",\n"
-          + "            \"request\": {\n"
-          + "                \"urlPathPattern\": \"/oauth/authorize.*\",\n"
-          + "                \"method\": \"GET\"\n"
-          + "            },\n"
-          + "            \"response\": {\n"
-          + "                \"status\": 200,\n"
-          + "                \"fixedDelayMilliseconds\": 5000\n"
-          + "            }\n"
-          + "        }\n"
-          + "    ],\n"
-          + "    \"importOptions\": {\n"
-          + "        \"duplicatePolicy\": \"IGNORE\",\n"
-          + "        \"deleteAllNotInImport\": true\n"
-          + "    }\n"
-          + "}";
-
+      SCENARIOS_BASE_DIR + "/browser_timeout_scenario_mapping.json";
   private static final String INVALID_SCOPE_SCENARIO_MAPPING =
-      "{\n"
-          + "    \"mappings\": [\n"
-          + "        {\n"
-          + "            \"scenarioName\": \"Invalid scope authorization error\",\n"
-          + "            \"request\": {\n"
-          + "                \"urlPathPattern\": \"/oauth/authorize.*\",\n"
-          + "                \"method\": \"GET\"\n"
-          + "            },\n"
-          + "            \"response\": {\n"
-          + "                \"status\": 200\n"
-          + "            },\n"
-          + "            \"serveEventListeners\": [\n"
-          + "                {\n"
-          + "                    \"name\": \"webhook\",\n"
-          + "                    \"parameters\": {\n"
-          + "                        \"method\": \"GET\",\n"
-          + "                        \"url\": \"http://localhost:8002/snowflake/oauth-redirect?error=invalid_scope&error_description=One+or+more+scopes+are+not+configured+for+the+authorization+server+resource.\"\n"
-          + "                    }\n"
-          + "                }\n"
-          + "            ]\n"
-          + "        }\n"
-          + "    ],\n"
-          + "    \"importOptions\": {\n"
-          + "        \"duplicatePolicy\": \"IGNORE\",\n"
-          + "        \"deleteAllNotInImport\": true\n"
-          + "    }\n"
-          + "}";
-
+      SCENARIOS_BASE_DIR + "/invalid_scope_scenario_mapping.json";
   private static final String TOKEN_REQUEST_ERROR_SCENARIO_MAPPING =
-      "{\n"
-          + "    \"mappings\": [\n"
-          + "        {\n"
-          + "            \"scenarioName\": \"OAuth token request error\",\n"
-          + "            \"requiredScenarioState\": \"Started\",\n"
-          + "            \"newScenarioState\": \"Authorized\",\n"
-          + "            \"request\": {\n"
-          + "                \"urlPathPattern\": \"/oauth/authorize.*\",\n"
-          + "                \"method\": \"GET\"\n"
-          + "            },\n"
-          + "            \"response\": {\n"
-          + "                \"status\": 200\n"
-          + "            },\n"
-          + "            \"serveEventListeners\": [\n"
-          + "                {\n"
-          + "                    \"name\": \"webhook\",\n"
-          + "                    \"parameters\": {\n"
-          + "                        \"method\": \"GET\",\n"
-          + "                        \"url\": \"http://localhost:8003/snowflake/oauth-redirect?code=123\"\n"
-          + "                    }\n"
-          + "                }\n"
-          + "            ]\n"
-          + "        },\n"
-          + "        {\n"
-          + "            \"scenarioName\": \"OAuth token request error\",\n"
-          + "            \"requiredScenarioState\": \"Authorized\",\n"
-          + "            \"newScenarioState\": \"Token request error\",\n"
-          + "            \"request\": {\n"
-          + "                \"urlPathPattern\": \"/oauth/token-request.*\",\n"
-          + "                \"method\": \"POST\",\n"
-          + "                \"headers\": {\n"
-          + "                    \"Authorization\": {\n"
-          + "                        \"contains\": \"Basic\"\n"
-          + "                    },\n"
-          + "                    \"Content-Type\": {\n"
-          + "                        \"contains\": \"application/x-www-form-urlencoded; charset=UTF-8\"\n"
-          + "                    }\n"
-          + "                },\n"
-          + "                \"bodyPatterns\": [{\n"
-          + "                    \"contains\": \"grant_type=authorization_code&code=123&redirect_uri=http%3A%2F%2Flocalhost%3A8003%2Fsnowflake%2Foauth-redirect&code_verifier=\"\n"
-          + "                }]\n"
-          + "            },\n"
-          + "            \"response\": {\n"
-          + "                \"status\": 400\n"
-          + "            }\n"
-          + "        }\n"
-          + "    ],\n"
-          + "    \"importOptions\": {\n"
-          + "        \"duplicatePolicy\": \"IGNORE\",\n"
-          + "        \"deleteAllNotInImport\": true\n"
-          + "    }\n"
-          + "}";
+      SCENARIOS_BASE_DIR + "/token_request_error_scenario_mapping.json";
 
   private static final Logger logger =
       LoggerFactory.getLogger(OauthAuthorizationCodeFlowLatestIT.class);
 
-  AuthExternalBrowserHandlers wiremockProxyRequestBrowserHandler =
+  private final AuthExternalBrowserHandlers wiremockProxyRequestBrowserHandler =
       new WiremockProxyRequestBrowserHandler();
 
   @Test
   public void successfulFlowScenario() throws SFException {
-    importMapping(SUCCESSFUL_FLOW_SCENARIO_MAPPINGS);
+    importMappingFromResources(SUCCESSFUL_FLOW_SCENARIO_MAPPINGS);
     SFLoginInput loginInput =
         createLoginInputStub("http://localhost:8001/snowflake/oauth-redirect");
 
@@ -208,7 +59,7 @@ public class OauthAuthorizationCodeFlowLatestIT extends BaseWiremockTest {
 
   @Test
   public void browserTimeoutFlowScenario() {
-    importMapping(BROWSER_TIMEOUT_SCENARIO_MAPPING);
+    importMappingFromResources(BROWSER_TIMEOUT_SCENARIO_MAPPING);
     SFLoginInput loginInput =
         createLoginInputStub("http://localhost:8004/snowflake/oauth-redirect");
 
@@ -224,7 +75,7 @@ public class OauthAuthorizationCodeFlowLatestIT extends BaseWiremockTest {
 
   @Test
   public void invalidScopeFlowScenario() {
-    importMapping(INVALID_SCOPE_SCENARIO_MAPPING);
+    importMappingFromResources(INVALID_SCOPE_SCENARIO_MAPPING);
     SFLoginInput loginInput =
         createLoginInputStub("http://localhost:8002/snowflake/oauth-redirect");
 
@@ -240,7 +91,7 @@ public class OauthAuthorizationCodeFlowLatestIT extends BaseWiremockTest {
 
   @Test
   public void tokenRequestErrorFlowScenario() {
-    importMapping(TOKEN_REQUEST_ERROR_SCENARIO_MAPPING);
+    importMappingFromResources(TOKEN_REQUEST_ERROR_SCENARIO_MAPPING);
     SFLoginInput loginInput =
         createLoginInputStub("http://localhost:8003/snowflake/oauth-redirect");
 
