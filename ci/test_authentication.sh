3c63219107a70001ada5fd26185c0e6ccd2db644
Adam Kolodziejczyk <adam.kolodziejczyk@snowflake.com>
2024-12-05 11:57:24 +0100

    add internal repo

--- ci/test_authentication.sh
+++ ci/test_authentication.sh
@@ -3,6 +3,7 @@
 set -o pipefail
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export WORKSPACE=${WORKSPACE:-/tmp}
+export INTERNAL_REPO=nexus.int.snowflakecomputing.com:8086
 
 source $THIS_DIR/scripts/login_internal_docker.sh
 gpg --quiet --batch --yes --decrypt --passphrase="$PARAMETERS_SECRET" --output $THIS_DIR/../.github/workflows/parameters_aws_auth_tests.json "$THIS_DIR/../.github/workflows/parameters_aws_auth_tests.json.gpg"
