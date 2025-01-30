2cbdbd2a7281dd65c40c032b4705a61724fc5768
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-01-30 14:29:59 +0100

    styling

--- src/test/java/net/snowflake/client/authentication/AuthConnectionParameters.java
+++ src/test/java/net/snowflake/client/authentication/AuthConnectionParameters.java
@@ -91,9 +91,11 @@ public class AuthConnectionParameters {
     Properties properties = getBaseConnectionParameters();
     properties.put("authenticator", "OAUTH_AUTHORIZATION_CODE");
     properties.put(
-            "clientId", systemGetEnv("SNOWFLAKE_AUTH_TEST_INTERNAL_OAUTH_SNOWFLAKE_WILDCARDS_CLIENT_ID"));
+        "clientId",
+        systemGetEnv("SNOWFLAKE_AUTH_TEST_INTERNAL_OAUTH_SNOWFLAKE_WILDCARDS_CLIENT_ID"));
     properties.put(
-            "clientSecret", systemGetEnv("SNOWFLAKE_AUTH_TEST_INTERNAL_OAUTH_SNOWFLAKE_WILDCARDS_CLIENT_SECRET"));
+        "clientSecret",
+        systemGetEnv("SNOWFLAKE_AUTH_TEST_INTERNAL_OAUTH_SNOWFLAKE_WILDCARDS_CLIENT_SECRET"));
     properties.put("role", systemGetEnv("SNOWFLAKE_AUTH_TEST_INTERNAL_OAUTH_SNOWFLAKE_ROLE"));
     properties.put("user", systemGetEnv("SNOWFLAKE_AUTH_TEST_EXTERNAL_OAUTH_OKTA_CLIENT_ID"));
     return properties;
