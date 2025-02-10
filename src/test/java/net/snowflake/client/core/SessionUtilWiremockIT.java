2bae032fea09819249368380d25b594cb34cd776
Filip Pawłowski <filip.pawlowski@snowflake.com>
2025-02-10 02:02:47 +0100

    SNOW-1853185: Replaced .* imports with specific

--- src/test/java/net/snowflake/client/core/SessionUtilWiremockIT.java
+++ src/test/java/net/snowflake/client/core/SessionUtilWiremockIT.java
@@ -3,7 +3,13 @@ package net.snowflake.client.core;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.*;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import net.snowflake.client.category.TestTags;
 import net.snowflake.client.jdbc.BaseWiremockTest;
