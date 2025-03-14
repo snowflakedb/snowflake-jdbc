e34b16c8afcb8a8542b35f7c8b4f1a18f29d4307
Przemyslaw Motacki <przemyslaw.motacki@snowflake.com>
2025-03-14 09:27:35 +0100

    SNOW-1963153 preparen ext development versions 3.23.2

--- src/main/java/net/snowflake/client/jdbc/SnowflakeDriver.java
+++ src/main/java/net/snowflake/client/jdbc/SnowflakeDriver.java
@@ -33,7 +33,7 @@ public class SnowflakeDriver implements Driver {
   static SnowflakeDriver INSTANCE;
 
   public static final Properties EMPTY_PROPERTIES = new Properties();
-  public static String implementVersion = "3.23.1";
+  public static String implementVersion = "3.23.2";
 
   static int majorVersion = 0;
   static int minorVersion = 0;
