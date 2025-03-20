7e3c872433f86b43e93418fd21e20065b642bafd
Bing Li <bing.li@snowflake.com>
2025-03-20 11:37:45 -0700

    move arg

---
+++ src/test/java/net/snowflake/client/jdbc/SnowparkTest.java
@@ -0,0 +1,12 @@
+package net.snowflake.client;
+
+import net.snowflake.client.jdbc.SnowflakeSQLException;
+import org.junit.jupiter.api.Test;
+
+public class SnowparkTest {
+
+    @Test
+    void test() {
+        
+    }
+}
