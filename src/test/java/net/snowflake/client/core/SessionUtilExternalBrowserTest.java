1ed179e793e844aae212dcc1b3cab1de65abd94f
sfc-gh-ext-simba-vb <Vikramsinh.Barbade@insightsoftware.com>
2025-06-17 14:39:29 -0700

    External browser changes reverted due to browser issue.

--- src/test/java/net/snowflake/client/core/SessionUtilExternalBrowserTest.java
+++ src/test/java/net/snowflake/client/core/SessionUtilExternalBrowserTest.java
@@ -1,7 +1,6 @@
 package net.snowflake.client.core;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -24,7 +23,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.Map;
 import net.snowflake.client.AbstractDriverIT;
-import net.snowflake.client.core.auth.AuthenticatorType;
 import net.snowflake.client.jdbc.SnowflakeBasicDataSource;
 import net.snowflake.client.jdbc.SnowflakeSQLException;
 import net.snowflake.client.jdbc.SnowflakeSQLLoggedException;
@@ -308,43 +306,5 @@ public class SessionUtilExternalBrowserTest {
               ds.getConnection();
             });
     assertTrue(e.getMessage().contains("External browser authentication failed"));
-  }
-
-  @Test
-  public void testSessionUtilExternalBrowserWithConsoleLogin() throws Throwable {
-    SFLoginInput loginInput = mock(SFLoginInput.class);
-    when(loginInput.getServerUrl()).thenReturn("https://testaccount.snowflakecomputing.com/");
-    when(loginInput.getAuthenticator()).thenReturn(AuthenticatorType.EXTERNALBROWSER.name());
-    when(loginInput.getAccountName()).thenReturn("testaccount");
-    when(loginInput.getUserName()).thenReturn("testuser");
-    when(loginInput.getDisableConsoleLogin()).thenReturn(false);
-
-    try (MockedStatic<HttpUtil> mockedHttpUtil = mockStatic(HttpUtil.class)) {
-      mockedHttpUtil
-          .when(
-              () ->
-                  HttpUtil.executeGeneralRequest(
-                      Mockito.any(HttpRequestBase.class),
-                      Mockito.anyInt(),
-                      Mockito.anyInt(),
-                      Mockito.anyInt(),
-                      Mockito.anyInt(),
-                      Mockito.nullable(HttpClientSettingsKey.class)))
-          .thenReturn(
-              "{\"success\":\"true\",\"data\":{\"proofKey\":\""
-                  + MOCK_PROOF_KEY
-                  + "\","
-                  + " \"ssoUrl\":\""
-                  + MOCK_SSO_URL
-                  + "\"}}");
-
-      SessionUtilExternalBrowser sub = FakeSessionUtilExternalBrowser.createInstance(loginInput);
-      sub.authenticate();
-      assertThat("", sub.getToken(), equalTo(FakeSessionUtilExternalBrowser.MOCK_SAML_TOKEN));
-
-      sub = FakeSessionUtilExternalBrowser.createInstance(loginInput, true);
-      sub.authenticate();
-      assertThat("", sub.getToken(), equalTo(FakeSessionUtilExternalBrowser.MOCK_SAML_TOKEN));
-    }
   }
 }
