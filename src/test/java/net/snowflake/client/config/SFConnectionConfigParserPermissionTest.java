993802182e54a3c6e94295f8c5c627606b3d2633
sfc-gh-ext-simba-vb <vikramsinh.barbade@insightsoftware.com>
2025-07-23 15:19:57 -0700

    converted to parameterized JUnit test.

--- src/test/java/net/snowflake/client/config/SFConnectionConfigParserPermissionTest.java
+++ src/test/java/net/snowflake/client/config/SFConnectionConfigParserPermissionTest.java
@@ -8,10 +8,14 @@ import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.PosixFilePermission;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import net.snowflake.client.annotations.DontRunOnWindows;
 import net.snowflake.client.jdbc.SnowflakeUtil;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SFConnectionConfigParserPermissionTest {
 
@@ -39,114 +43,103 @@ class SFConnectionConfigParserPermissionTest {
     }
   }
 
-  @Test
-  @DontRunOnWindows
-  void testGroupWritePermissionRaisesException() throws Exception {
-    Set<PosixFilePermission> perms =
-        Set.of(
-            PosixFilePermission.OWNER_READ,
-            PosixFilePermission.OWNER_WRITE,
-            PosixFilePermission.GROUP_WRITE);
-    Path tempFile = createTempFileWithPermissions(perms);
-    Exception ex =
-        assertThrows(
-            net.snowflake.client.jdbc.SnowflakeSQLException.class,
-            () -> invokeVerifyFilePermissionSecure(tempFile));
-
-    assertTrue(ex.getMessage().contains("writable by group or others"));
-    Files.deleteIfExists(tempFile);
+  static List<Object[]> permissionTestCases() {
+    return Arrays.asList(
+        new Object[][] {
+          { // Group write
+            new HashSet<>(
+                Arrays.asList(
+                    PosixFilePermission.OWNER_READ,
+                    PosixFilePermission.OWNER_WRITE,
+                    PosixFilePermission.GROUP_WRITE)),
+            true,
+            "writable by group or others"
+          },
+          { // Others write
+            new HashSet<>(
+                Arrays.asList(
+                    PosixFilePermission.OWNER_READ,
+                    PosixFilePermission.OWNER_WRITE,
+                    PosixFilePermission.OTHERS_WRITE)),
+            true,
+            "writable by group or others"
+          },
+          { // Owner execute
+            new HashSet<>(
+                Arrays.asList(
+                    PosixFilePermission.OWNER_READ,
+                    PosixFilePermission.OWNER_WRITE,
+                    PosixFilePermission.OWNER_EXECUTE)),
+            true,
+            "executable"
+          },
+          { // Group execute
+            new HashSet<>(
+                Arrays.asList(
+                    PosixFilePermission.OWNER_READ,
+                    PosixFilePermission.OWNER_WRITE,
+                    PosixFilePermission.GROUP_EXECUTE)),
+            true,
+            "executable"
+          },
+          { // Others execute
+            new HashSet<>(
+                Arrays.asList(
+                    PosixFilePermission.OWNER_READ,
+                    PosixFilePermission.OWNER_WRITE,
+                    PosixFilePermission.OTHERS_EXECUTE)),
+            true,
+            "executable"
+          },
+          { // Owner read/write only
+            new HashSet<>(
+                Arrays.asList(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE)),
+            false,
+            null
+          }
+        });
   }
 
-  @Test
+  @ParameterizedTest
+  @MethodSource("permissionTestCases")
   @DontRunOnWindows
-  void testOthersWritePermissionRaisesException() throws Exception {
-    Set<PosixFilePermission> perms =
-        Set.of(
-            PosixFilePermission.OWNER_READ,
-            PosixFilePermission.OWNER_WRITE,
-            PosixFilePermission.OTHERS_WRITE);
+  void testFilePermissionScenarios(
+      Set<PosixFilePermission> perms, boolean shouldThrow, String expectedMsg) throws Exception {
     Path tempFile = createTempFileWithPermissions(perms);
-    Exception ex =
-        assertThrows(
-            net.snowflake.client.jdbc.SnowflakeSQLException.class,
-            () -> invokeVerifyFilePermissionSecure(tempFile));
-
-    assertTrue(ex.getMessage().contains("writable by group or others"));
-    Files.deleteIfExists(tempFile);
+    try {
+      if (shouldThrow) {
+        Exception ex =
+            assertThrows(
+                net.snowflake.client.jdbc.SnowflakeSQLException.class,
+                () -> invokeVerifyFilePermissionSecure(tempFile));
+        assertTrue(ex.getMessage().contains(expectedMsg));
+      } else {
+        assertDoesNotThrow(() -> invokeVerifyFilePermissionSecure(tempFile));
+      }
+    } finally {
+      Files.deleteIfExists(tempFile);
+    }
   }
 
-  @Test
-  @DontRunOnWindows
-  void testOwnerExecutePermissionRaisesException() throws Exception {
-    Set<PosixFilePermission> perms =
-        Set.of(
-            PosixFilePermission.OWNER_READ,
-            PosixFilePermission.OWNER_WRITE,
-            PosixFilePermission.OWNER_EXECUTE);
-    Path tempFile = createTempFileWithPermissions(perms);
-    Exception ex =
-        assertThrows(
-            net.snowflake.client.jdbc.SnowflakeSQLException.class,
-            () -> invokeVerifyFilePermissionSecure(tempFile));
-    assertTrue(ex.getMessage().contains("executable"));
-    Files.deleteIfExists(tempFile);
+  static List<Object[]> skipReadWarningTestCases() {
+    return Arrays.asList(
+        new Object[][] {
+          {
+            new HashSet<>(
+                Arrays.asList(
+                    PosixFilePermission.OWNER_READ,
+                    PosixFilePermission.OWNER_WRITE,
+                    PosixFilePermission.GROUP_READ,
+                    PosixFilePermission.OTHERS_READ))
+          }
+        });
   }
 
-  @Test
+  @ParameterizedTest
+  @MethodSource("skipReadWarningTestCases")
   @DontRunOnWindows
-  void testGroupExecutePermissionRaisesException() throws Exception {
-    Set<PosixFilePermission> perms =
-        Set.of(
-            PosixFilePermission.OWNER_READ,
-            PosixFilePermission.OWNER_WRITE,
-            PosixFilePermission.GROUP_EXECUTE);
-    Path tempFile = createTempFileWithPermissions(perms);
-    Exception ex =
-        assertThrows(
-            net.snowflake.client.jdbc.SnowflakeSQLException.class,
-            () -> invokeVerifyFilePermissionSecure(tempFile));
-    assertTrue(ex.getMessage().contains("executable"));
-    Files.deleteIfExists(tempFile);
-  }
-
-  @Test
-  @DontRunOnWindows
-  void testOthersExecutePermissionRaisesException() throws Exception {
-    Set<PosixFilePermission> perms =
-        Set.of(
-            PosixFilePermission.OWNER_READ,
-            PosixFilePermission.OWNER_WRITE,
-            PosixFilePermission.OTHERS_EXECUTE);
-    Path tempFile = createTempFileWithPermissions(perms);
-    Exception ex =
-        assertThrows(
-            net.snowflake.client.jdbc.SnowflakeSQLException.class,
-            () -> invokeVerifyFilePermissionSecure(tempFile));
-    assertTrue(ex.getMessage().contains("executable"));
-    Files.deleteIfExists(tempFile);
-  }
-
-  @Test
-  @DontRunOnWindows
-  void testOwnerReadWritePermissionDoesNotRaiseException() throws Exception {
-    Set<PosixFilePermission> perms =
-        Set.of(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE);
-    Path tempFile = createTempFileWithPermissions(perms);
-    assertDoesNotThrow(() -> invokeVerifyFilePermissionSecure(tempFile));
-    Files.deleteIfExists(tempFile);
-  }
-
-  @Test
-  @DontRunOnWindows
-  void testSkipWarningForReadPermissionsEnvVar() throws Exception {
-    // Set the environment variable to skip read permission warnings
+  void testSkipWarningForReadPermissionsEnvVar(Set<PosixFilePermission> perms) throws Exception {
     SnowflakeUtil.systemSetEnv("SF_SKIP_WARNING_FOR_READ_PERMISSIONS_ON_CONFIG_FILE", "true");
-    Set<PosixFilePermission> perms =
-        Set.of(
-            PosixFilePermission.OWNER_READ,
-            PosixFilePermission.OWNER_WRITE,
-            PosixFilePermission.GROUP_READ,
-            PosixFilePermission.OTHERS_READ);
     Path tempFile = createTempFileWithPermissions(perms);
     try {
       assertDoesNotThrow(() -> invokeVerifyFilePermissionSecure(tempFile));
