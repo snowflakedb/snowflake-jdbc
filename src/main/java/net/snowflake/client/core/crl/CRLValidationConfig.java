134aa40d0b63dfc9cda75c94ecf10bcb0fc5aa74
Rafal Kowalski <rafal.kowalski@snowflake.com>
2025-08-04 15:13:40 +0200

    SNOW-2117143: Review fixes

--- src/main/java/net/snowflake/client/core/crl/CRLValidationConfig.java
+++ src/main/java/net/snowflake/client/core/crl/CRLValidationConfig.java
@@ -1,10 +1,10 @@
 package net.snowflake.client.core.crl;
 
 /** Configuration parameters for CRL validation. */
-public class CRLValidationConfig {
+class CRLValidationConfig {
 
   /** Certificate revocation check mode. */
-  public enum CertRevocationCheckMode {
+  enum CertRevocationCheckMode {
     /** Disables CRL checking (with TLS handshake still in place) */
     DISABLED,
 
@@ -33,53 +33,53 @@ public class CRLValidationConfig {
     this.readTimeoutMs = builder.readTimeoutMs;
   }
 
-  public CertRevocationCheckMode getCertRevocationCheckMode() {
+  CertRevocationCheckMode getCertRevocationCheckMode() {
     return certRevocationCheckMode;
   }
 
-  public boolean isAllowCertificatesWithoutCrlUrl() {
+  boolean isAllowCertificatesWithoutCrlUrl() {
     return allowCertificatesWithoutCrlUrl;
   }
 
-  public int getConnectionTimeoutMs() {
+  int getConnectionTimeoutMs() {
     return connectionTimeoutMs;
   }
 
-  public int getReadTimeoutMs() {
+  int getReadTimeoutMs() {
     return readTimeoutMs;
   }
 
-  public static Builder builder() {
+  static Builder builder() {
     return new Builder();
   }
 
-  public static class Builder {
+  static class Builder {
     private CertRevocationCheckMode certRevocationCheckMode = CertRevocationCheckMode.DISABLED;
     private boolean allowCertificatesWithoutCrlUrl = false;
     private int connectionTimeoutMs = 30000; // 30 seconds
     private int readTimeoutMs = 30000; // 30 seconds
 
-    public Builder certRevocationCheckMode(CertRevocationCheckMode mode) {
+    Builder certRevocationCheckMode(CertRevocationCheckMode mode) {
       this.certRevocationCheckMode = mode;
       return this;
     }
 
-    public Builder allowCertificatesWithoutCrlUrl(boolean allow) {
+    Builder allowCertificatesWithoutCrlUrl(boolean allow) {
       this.allowCertificatesWithoutCrlUrl = allow;
       return this;
     }
 
-    public Builder connectionTimeoutMs(int timeoutMs) {
+    Builder connectionTimeoutMs(int timeoutMs) {
       this.connectionTimeoutMs = timeoutMs;
       return this;
     }
 
-    public Builder readTimeoutMs(int timeoutMs) {
+    Builder readTimeoutMs(int timeoutMs) {
       this.readTimeoutMs = timeoutMs;
       return this;
     }
 
-    public CRLValidationConfig build() {
+    CRLValidationConfig build() {
       return new CRLValidationConfig(this);
     }
   }
--- src/main/java/net/snowflake/client/core/crl/CertificateValidationResult.java
+++ src/main/java/net/snowflake/client/core/crl/CertificateValidationResult.java
@@ -1,13 +1,7 @@
 package net.snowflake.client.core.crl;
 
-/** Represents the result of CRL validation for an individual certificate. */
-public enum CertificateValidationResult {
-  /** Certificate is not revoked according to CRL */
+enum CertificateValidationResult {
   CERT_UNREVOKED,
-
-  /** Certificate is revoked according to CRL */
   CERT_REVOKED,
-
-  /** Error occurred during CRL validation for this certificate */
   CERT_ERROR
 }
