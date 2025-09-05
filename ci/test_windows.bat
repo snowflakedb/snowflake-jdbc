022a938cda0b2c8e0a7b32ec3ca8986e5128d9d2
Patryk Cyrek <patryk.cyrek@snowflake.com>
2025-09-05 13:30:29 +0200

    extension fix

--- ci/test_windows.bat
+++ ci/test_windows.bat
@@ -11,20 +11,20 @@ cd %GITHUB_WORKSPACE%
 
 if "%CLOUD_PROVIDER%"=="AZURE" (
   set ENCODED_PARAMETERS_FILE=.github/workflows/parameters_azure.json.gpg
-  set ENCODED_RSA_KEY_FILE=.github/workflows/rsa/keys/rsa_key_jdbc_azure.json.gpg
+  set ENCODED_RSA_KEY_FILE=.github/workflows/rsa/keys/rsa_key_jdbc_azure.p8.gpg
 ) else if "%CLOUD_PROVIDER%"=="GCP" (
   set ENCODED_PARAMETERS_FILE=.github/workflows/parameters_gcp.json.gpg
-  set ENCODED_RSA_KEY_FILE=.github/workflows/rsa/keys/rsa_key_jdbc_gcp.json.gpg
+  set ENCODED_RSA_KEY_FILE=.github/workflows/rsa/keys/rsa_key_jdbc_gcp.p8.gpg
 ) else if "%CLOUD_PROVIDER%"=="AWS" (
   set ENCODED_PARAMETERS_FILE=.github/workflows/parameters_aws.json.gpg
-  set ENCODED_RSA_KEY_FILE=.github/workflows/rsa/keys/rsa_key_jdbc_aws.json.gpg
+  set ENCODED_RSA_KEY_FILE=.github/workflows/rsa/keys/rsa_key_jdbc_aws.p8.gpg
 ) else (
   echo === unknown cloud provider
   exit /b 1
 )
 
 gpg --quiet --batch --yes --decrypt --passphrase=%PARAMETERS_SECRET% --output parameters.json %ENCODED_PARAMETERS_FILE%
-gpg --quiet --batch --yes --decrypt --passphrase=%JDBC_PRIVATE_KEY_SECRET% --output rsa_key_jdbc.json %ENCODED_RSA_KEY_FILE%
+gpg --quiet --batch --yes --decrypt --passphrase=%JDBC_PRIVATE_KEY_SECRET% --output rsa_key_jdbc.p8 %ENCODED_RSA_KEY_FILE%
 
 REM DON'T FORGET TO include @echo off here or the password may be leaked!
 echo @echo off>parameters.bat
