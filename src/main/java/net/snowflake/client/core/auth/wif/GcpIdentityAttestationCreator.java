73e04c4e73ce21c227d03a99657de44771437a7e
Rafal Kowalski <rafal.kowalski@snowflake.com>
2025-09-18 16:24:49 +0200

    SNOW-2236575: Support WIF Impersonation on GCP workloads

--- src/main/java/net/snowflake/client/core/auth/wif/GcpIdentityAttestationCreator.java
+++ src/main/java/net/snowflake/client/core/auth/wif/GcpIdentityAttestationCreator.java
@@ -3,20 +3,32 @@ package net.snowflake.client.core.auth.wif;
 import static net.snowflake.client.core.auth.wif.WorkloadIdentityUtil.DEFAULT_METADATA_SERVICE_BASE_URL;
 import static net.snowflake.client.core.auth.wif.WorkloadIdentityUtil.performIdentityRequest;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import net.snowflake.client.core.SFException;
 import net.snowflake.client.core.SFLoginInput;
 import net.snowflake.client.core.SnowflakeJdbcInternalApi;
 import net.snowflake.client.jdbc.ErrorCode;
+import net.snowflake.client.jdbc.SnowflakeUtil;
 import net.snowflake.client.log.SFLogger;
 import net.snowflake.client.log.SFLoggerFactory;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 
 @SnowflakeJdbcInternalApi
 public class GcpIdentityAttestationCreator implements WorkloadIdentityAttestationCreator {
 
   private static final String METADATA_FLAVOR_HEADER_NAME = "Metadata-Flavor";
   private static final String METADATA_FLAVOR = "Google";
+  private static final ObjectMapper objectMapper = new ObjectMapper();
 
   private final String gcpMetadataServiceBaseUrl;
 
@@ -27,7 +39,7 @@ public class GcpIdentityAttestationCreator implements WorkloadIdentityAttestatio
 
   public GcpIdentityAttestationCreator(SFLoginInput loginInput) {
     this.loginInput = loginInput;
-    gcpMetadataServiceBaseUrl = DEFAULT_METADATA_SERVICE_BASE_URL;
+    this.gcpMetadataServiceBaseUrl = DEFAULT_METADATA_SERVICE_BASE_URL;
   }
 
   /** Only for testing purpose */
@@ -38,8 +50,16 @@ public class GcpIdentityAttestationCreator implements WorkloadIdentityAttestatio
 
   @Override
   public WorkloadIdentityAttestation createAttestation() throws SFException {
-    logger.debug("Creating GCP identity attestation...");
-    String token = fetchTokenFromMetadataService();
+    String token;
+
+    if (SnowflakeUtil.isNullOrEmpty(loginInput.getWorkloadIdentityImpersonationPath())) {
+      logger.debug("Creating GCP identity attestation...");
+      token = fetchTokenFromMetadataService();
+    } else {
+      logger.debug("Creating GCP identity attestation with impersonation...");
+      token = getGcpIdentityTokenViaImpersonation();
+    }
+
     if (token == null) {
       throw new SFException(ErrorCode.WORKLOAD_IDENTITY_FLOW_ERROR, "No GCP token was found.");
     }
@@ -67,6 +87,46 @@ public class GcpIdentityAttestationCreator implements WorkloadIdentityAttestatio
       throw new SFException(
           ErrorCode.WORKLOAD_IDENTITY_FLOW_ERROR,
           "GCP metadata server request was not successful: " + e.getMessage());
+    }
+  }
+
+  private String getGcpIdentityTokenViaImpersonation() throws SFException {
+    String accessToken = fetchTokenFromMetadataService();
+    List<String> impersonationPath =
+        Arrays.asList(loginInput.getWorkloadIdentityImpersonationPath().split(","));
+
+    List<String> fullServiceAccountPaths =
+        impersonationPath.stream()
+            .map(arn -> "projects/-/serviceAccounts/" + arn)
+            .collect(Collectors.toList());
+
+    String targetServiceAccount = fullServiceAccountPaths.get(fullServiceAccountPaths.size() - 1);
+    List<String> delegates = fullServiceAccountPaths.subList(0, fullServiceAccountPaths.size() - 1);
+
+    String url =
+        String.format(
+            "https://iamcredentials.googleapis.com/v1/%s:generateIdToken", targetServiceAccount);
+
+    HttpPost request = new HttpPost(url);
+    request.setHeader("Authorization", "Bearer " + accessToken);
+    request.setHeader("Content-Type", "application/json");
+
+    Map<String, Object> requestBody = new HashMap<>();
+    requestBody.put("delegates", delegates);
+    requestBody.put("audience", WorkloadIdentityUtil.SNOWFLAKE_AUDIENCE);
+
+    try {
+      String json = objectMapper.writeValueAsString(requestBody);
+      request.setEntity(new StringEntity(json, StandardCharsets.UTF_8));
+
+      String response = WorkloadIdentityUtil.performIdentityRequest(request, loginInput);
+      JsonNode responseBody = objectMapper.readTree(response);
+      return responseBody.get("token").asText();
+    } catch (Exception e) {
+      logger.error("Error fetching GCP impersonated identity token", e);
+      throw new SFException(
+          ErrorCode.WORKLOAD_IDENTITY_FLOW_ERROR,
+          "Error fetching GCP impersonated identity token: " + e.getMessage());
     }
   }
 }
