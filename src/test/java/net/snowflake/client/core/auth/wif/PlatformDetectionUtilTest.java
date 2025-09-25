2fc1b4db259679c5da7ad85feff3e39bca5b38b4
Dawid Heyman <dawid.heyman@snowflake.com>
2025-09-25 12:09:08 +0200

    checkstyle

--- src/test/java/net/snowflake/client/core/auth/wif/PlatformDetectionUtilTest.java
+++ src/test/java/net/snowflake/client/core/auth/wif/PlatformDetectionUtilTest.java
@@ -1,7 +1,9 @@
 package net.snowflake.client.core.auth.wif;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
